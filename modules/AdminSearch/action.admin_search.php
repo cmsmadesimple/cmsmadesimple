b989a34ce0c06b3ec8d120622697ec905590f0b9
calguy1000 <calguy1000_old@cmsmadesimple.org>
2015-04-18 18:20:07 +0000

    minor fix.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@9923 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/AdminSearch/action.admin_search.php
+++ modules/AdminSearch/action.admin_search.php
@@ -21,7 +21,7 @@
 if( !isset($gCms) ) exit;
 if( !$this->VisibleToAdminUser() ) exit;
 
-function status_error($msg) 
+function status_error($msg)
 {
   echo '<script type="text/javascript">parent.status_error(\''.$msg.'\')</script>';
 }
@@ -63,28 +63,30 @@ unset($searchparams['slaves']);
 status_msg($this->Lang('starting'));
 $slaves = AdminSearch_tools::get_slave_classes();
 if( is_array($slaves) && count($slaves) ) {
-  foreach( $slaves as $one_slave ) {
-    if( !in_array($one_slave['class'],$params['slaves']) ) continue;
-    $module = cms_utils::get_module($one_slave['module']);
-    if( !is_object($module) ) continue;
-    if( !class_exists($one_slave['class']) ) continue;
-    if( !is_subclass_of($one_slave['class'],'AdminSearch_slave') ) continue;
+    foreach( $slaves as $one_slave ) {
+        if( !in_array($one_slave['class'],$params['slaves']) ) continue;
+        $module = cms_utils::get_module($one_slave['module']);
+        if( !is_object($module) ) continue;
+        if( !class_exists($one_slave['class']) ) continue;
+        if( !is_subclass_of($one_slave['class'],'AdminSearch_slave') ) continue;
 
-    $obj = new $one_slave['class'];
-    if( !is_object($obj) ) continue;
-    if( !$obj->check_permission() ) continue;
+        $obj = new $one_slave['class'];
+        if( !is_object($obj) ) continue;
+        if( !$obj->check_permission() ) continue;
 
-    $obj->set_params($searchparams);
-    $results = $obj->get_matches();
-    if( is_array($results) && count($results) ) {
-      begin_section($one_slave['class'],$obj->get_name(),$obj->get_section_description());
-      foreach( $results as $one ) {
-	$text = isset($one['text'])?$one['text']:'';
-	add_result($one_slave['class'],$one['title'],$one['description'],isset($one['edit_url'])?$one['edit_url']:'',$text);
-      }
+        $obj->set_params($searchparams);
+        $results = $obj->get_matches();
+        if( is_array($results) && count($results) ) {
+            begin_section($one_slave['class'],$obj->get_name(),$obj->get_section_description());
+            foreach( $results as $one ) {
+                $text = isset($one['text'])?$one['text']:'';
+                add_result($one_slave['class'],$one['title'],
+                           isset($one['description'])?$one['description']:'',
+                           isset($one['edit_url'])?$one['edit_url']:'',$text);
+            }
+        }
+        end_section();
     }
-    end_section();
-  }
 }
 status_msg($this->Lang('finished'));
 exit;
