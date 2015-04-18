a4a02cb0e4b26835a0b8a7420a6cc482b2e6d5d0
calguy1000 <calguy1000_old@cmsmadesimple.org>
2015-04-18 18:20:21 +0000

    minor fix.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@9924 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/CMSContentManager/action.admin_pagedefaults_tab.php
+++ modules/CMSContentManager/action.admin_pagedefaults_tab.php
@@ -1,10 +1,10 @@
 <?php
 #BEGIN_LICENSE
 #-------------------------------------------------------------------------
-# Module: Content (c) 2013 by Robert Campbell 
+# Module: Content (c) 2013 by Robert Campbell
 #         (calguy1000@cmsmadesimple.org)
 #  A module for managing content in CMSMS.
-# 
+#
 #-------------------------------------------------------------------------
 # CMS - CMS Made Simple is (c) 2004 by Ted Kulp (wishy@cmsmadesimple.org)
 # Visit our homepage at: http://www.cmsmadesimple.org
@@ -19,7 +19,7 @@
 # However, as a special exception to the GPL, this software is distributed
 # as an addon module to CMS Made Simple.  You may not use this software
 # in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin 
+# Made simple that does not indicate clearly and obviously in its admin
 # section that the site was built with CMS Made simple.
 #
 # This program is distributed in the hope that it will be useful,
@@ -40,25 +40,25 @@ $page_prefs = CmsContentManagerUtils::get_pagedefaults();
 $this->SetCurrentTab('pagedefaults');
 
 if( isset($params['pagedefaults']) && isset($params['submit']) ) {
-  // get settings from params
-  $modified = 0;
-  foreach( array_keys($page_prefs) as $fld ) {
-    if( isset($params[$fld]) ) {
-      $page_prefs[$fld] = $params[$fld];
-      $modified++;
+    // get settings from params
+    $modified = 0;
+    foreach( array_keys($page_prefs) as $fld ) {
+        if( isset($params[$fld]) ) {
+            $page_prefs[$fld] = $params[$fld];
+            $modified++;
+        }
     }
-  }
-  if( $modified ) {
-    // verify
-    if( in_array($page_prefs['contenttype'],$page_prefs['disallowed_types']) ) {
-      $this->SetError($this->Lang('error_contenttype_disallowed'));
+    if( $modified ) {
+        // verify
+        if( is_array($page_prefs['disallowed_types']) && in_array($page_prefs['contenttype'],$page_prefs['disallowed_types']) ) {
+            $this->SetError($this->Lang('error_contenttype_disallowed'));
+        }
+        else {
+            // save
+            $this->SetPreference('page_prefs',serialize($page_prefs));
+            $this->SetMessage($this->Lang('msg_prefs_saved'));
+        }
     }
-    else {
-      // save
-      $this->SetPreference('page_prefs',serialize($page_prefs));
-      $this->SetMessage($this->Lang('msg_prefs_saved'));
-    }
-  }
 }
 
 $this->RedirectToAdminTab('','','admin_settings');
