fd40c3a86e4f01211e7fd617552ddc2c1a489058
calguy1000 <calguy1000_old@cmsmadesimple.org>
2015-05-21 15:03:28 +0000

    minor.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@9974 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/Navigator/method.install.php
+++ modules/Navigator/method.install.php
@@ -38,141 +38,139 @@ if (!isset($gCms)) exit;
 
 $uid = null;
 if( cmsms()->test_state(CmsApp::STATE_INSTALL) ) {
-  $uid = 1; // hardcode to first user
+    $uid = 1; // hardcode to first user
 } else {
-  $uid = get_userid();
+    $uid = get_userid();
 }
 
 try {
-  $menu_template_type = new CmsLayoutTemplateType();
-  $menu_template_type->set_originator($this->GetName());
-  $menu_template_type->set_name('navigation');
-  $menu_template_type->set_dflt_flag(TRUE);
-  $menu_template_type->set_lang_callback('Navigator::page_type_lang_callback');
-  $menu_template_type->set_content_callback('Navigator::reset_page_type_defaults');
-  $menu_template_type->reset_content_to_factory();
-  $menu_template_type->save();
+    $menu_template_type = new CmsLayoutTemplateType();
+    $menu_template_type->set_originator($this->GetName());
+    $menu_template_type->set_name('navigation');
+    $menu_template_type->set_dflt_flag(TRUE);
+    $menu_template_type->set_lang_callback('Navigator::page_type_lang_callback');
+    $menu_template_type->set_content_callback('Navigator::reset_page_type_defaults');
+    $menu_template_type->reset_content_to_factory();
+    $menu_template_type->save();
 }
 catch( CmsException $e ) {
     // log it
-    debug_display($e); die();
-  debug_to_log(__FILE__.':'.__LINE__.' '.$e->GetMessage());
-  audit('',$this->GetName(),'Installation Error: '.$e->GetMessage());
-  return $e->GetMessage();
+    debug_to_log(__FILE__.':'.__LINE__.' '.$e->GetMessage());
+    audit('',$this->GetName(),'Installation Error: '.$e->GetMessage());
+    return $e->GetMessage();
 }
 
 try {
-  $bc_template_type = new CmsLayoutTemplateType();
-  $bc_template_type->set_originator($this->GetName());
-  $bc_template_type->set_name('breadcrumbs');
-  $bc_template_type->set_dflt_flag(TRUE);
-  $bc_template_type->set_lang_callback('Navigator::page_type_lang_callback');
-  $bc_template_type->set_content_callback('Navigator::reset_page_type_defaults');
-  $bc_template_type->reset_content_to_factory();
-  $bc_template_type->save();
+    $bc_template_type = new CmsLayoutTemplateType();
+    $bc_template_type->set_originator($this->GetName());
+    $bc_template_type->set_name(CmsLayoutTemplate::generate_unique_name('breadcrumbs'));
+    $bc_template_type->set_dflt_flag(TRUE);
+    $bc_template_type->set_lang_callback('Navigator::page_type_lang_callback');
+    $bc_template_type->set_content_callback('Navigator::reset_page_type_defaults');
+    $bc_template_type->reset_content_to_factory();
+    $bc_template_type->save();
 }
 catch( CmsException $e ) {
-  // log it
-  debug_to_log(__FILE__.':'.__LINE__.' '.$e->GetMessage());
-  audit('',$this->GetName(),'Installation Error: '.$e->GetMessage());
-  return $e->GetMessage();
+    // log it
+    debug_to_log(__FILE__.':'.__LINE__.' '.$e->GetMessage());
+    audit('',$this->GetName(),'Installation Error: '.$e->GetMessage());
+    return $e->GetMessage();
 }
 
 try {
-  $fn = cms_join_path(dirname(__FILE__),'templates','simple_navigation.tpl');
-  if( file_exists( $fn ) ) {
-    $template = @file_get_contents($fn);
-    $tpl = new CmsLayoutTemplate();
-    $tpl->set_name('Simple Navigation');
-    $tpl->set_owner($uid);
-    $tpl->set_content($template);
-    $tpl->set_type($menu_template_type);
-    $tpl->set_type_dflt(TRUE);
-    $tpl->save();
-  }
+    $fn = cms_join_path(dirname(__FILE__),'templates','simple_navigation.tpl');
+    if( file_exists( $fn ) ) {
+        $template = @file_get_contents($fn);
+        $tpl = new CmsLayoutTemplate();
+        $tpl->set_name(\CmsLayoutTemplate::generate_unique_name('Simple Navigation'));
+        $tpl->set_owner($uid);
+        $tpl->set_content($template);
+        $tpl->set_type($menu_template_type);
+        $tpl->set_type_dflt(TRUE);
+        $tpl->save();
+    }
 
-  $fn = cms_join_path(dirname(__FILE__),'templates','dflt_breadcrumbs.tpl');
-  if( file_exists( $fn ) ) {
-    $template = @file_get_contents($fn);
-    $tpl = new CmsLayoutTemplate();
-    $tpl->set_name('Breadcrumbs');
-    $tpl->set_owner($uid);
-    $tpl->set_content($template);
-    $tpl->set_type($bc_template_type);
-    $tpl->set_type_dflt(TRUE);
-    $tpl->save();
-  }
+    $fn = cms_join_path(dirname(__FILE__),'templates','dflt_breadcrumbs.tpl');
+    if( file_exists( $fn ) ) {
+        $template = @file_get_contents($fn);
+        $tpl = new CmsLayoutTemplate();
+        $tpl->set_name(\CmsLayoutTemplate::generate_unique_name('Breadcrumbs'));
+        $tpl->set_owner($uid);
+        $tpl->set_content($template);
+        $tpl->set_type($bc_template_type);
+        $tpl->set_type_dflt(TRUE);
+        $tpl->save();
+    }
 
-  $fn = cms_join_path(dirname(__FILE__),'templates','cssmenu.tpl');
-  if( file_exists( $fn ) ) {
-    $template = @file_get_contents($fn);
-    $tpl = new CmsLayoutTemplate();
-    $tpl->set_name('cssmenu');
-    $tpl->set_owner($uid);
-    $tpl->set_content($template);
-    $tpl->set_type($menu_template_type);
-    $tpl->set_type_dflt(TRUE);
-    $tpl->save();
-  }
+    $fn = cms_join_path(dirname(__FILE__),'templates','cssmenu.tpl');
+    if( file_exists( $fn ) ) {
+        $template = @file_get_contents($fn);
+        $tpl = new CmsLayoutTemplate();
+        $tpl->set_name(CmsLayoutTemplate::generate_unique_name('cssmenu'));
+        $tpl->set_owner($uid);
+        $tpl->set_content($template);
+        $tpl->set_type($menu_template_type);
+        $tpl->set_type_dflt(TRUE);
+        $tpl->save();
+    }
 
-  $fn = cms_join_path(dirname(__FILE__),'templates','cssmenu_ulshadow.tpl');
-  if( file_exists( $fn ) ) {
-    $template = @file_get_contents($fn);
-    $tpl = new CmsLayoutTemplate();
-    $tpl->set_name('cssmenu_ulshadow');
-    $tpl->set_owner($uid);
-    $tpl->set_content($template);
-    $tpl->set_type($menu_template_type);
-    $tpl->set_type_dflt(TRUE);
-    $tpl->save();
-  }
+    $fn = cms_join_path(dirname(__FILE__),'templates','cssmenu_ulshadow.tpl');
+    if( file_exists( $fn ) ) {
+        $template = @file_get_contents($fn);
+        $tpl = new CmsLayoutTemplate();
+        $tpl->set_name('cssmenu_ulshadow');
+        $tpl->set_owner($uid);
+        $tpl->set_content($template);
+        $tpl->set_type($menu_template_type);
+        $tpl->set_type_dflt(TRUE);
+        $tpl->save();
+    }
 
-  $fn = cms_join_path(dirname(__FILE__),'templates','minimal_menu.tpl');
-  if( file_exists( $fn ) ) {
-    $template = @file_get_contents($fn);
-    $tpl = new CmsLayoutTemplate();
-    $tpl->set_name('minimal_menu');
-    $tpl->set_owner($uid);
-    $tpl->set_content($template);
-    $tpl->set_type($menu_template_type);
-    $tpl->set_type_dflt(TRUE);
-    $tpl->save();
-  }
+    $fn = cms_join_path(dirname(__FILE__),'templates','minimal_menu.tpl');
+    if( file_exists( $fn ) ) {
+        $template = @file_get_contents($fn);
+        $tpl = new CmsLayoutTemplate();
+        $tpl->set_name('minimal_menu');
+        $tpl->set_owner($uid);
+        $tpl->set_content($template);
+        $tpl->set_type($menu_template_type);
+        $tpl->set_type_dflt(TRUE);
+        $tpl->save();
+    }
 
-  try {
-      $simplex = CmsLayoutCollection::load('Simplex');
+    try {
+        $simplex = CmsLayoutCollection::load('Simplex');
 
-      $fn = cms_join_path(dirname(__FILE__),'templates','Simplex_Main_Navigation.tpl');
-      if( file_exists( $fn ) ) {
-          $template = @file_get_contents($fn);
-          $tpl = new CmsLayoutTemplate();
-          $tpl->set_name('Simplex Main Navigation');
-          $tpl->set_owner($uid);
-          $tpl->set_content($template);
-          $tpl->set_type($menu_template_type);
-          $tpl->set_type_dflt(TRUE);
-          $tpl->add_design($simplex);
-          $tpl->save();
-      }
+        $fn = cms_join_path(dirname(__FILE__),'templates','Simplex_Main_Navigation.tpl');
+        if( file_exists( $fn ) ) {
+            $template = @file_get_contents($fn);
+            $tpl = new CmsLayoutTemplate();
+            $tpl->set_name('Simplex Main Navigation');
+            $tpl->set_owner($uid);
+            $tpl->set_content($template);
+            $tpl->set_type($menu_template_type);
+            $tpl->set_type_dflt(TRUE);
+            $tpl->add_design($simplex);
+            $tpl->save();
+        }
 
-      $fn = cms_join_path(dirname(__FILE__),'templates','Simplex_Footer_Navigation.tpl');
-      if( file_exists( $fn ) ) {
-          $template = @file_get_contents($fn);
-          $tpl = new CmsLayoutTemplate();
-          $tpl->set_name('Simplex Footer Navigation');
-          $tpl->set_owner($uid);
-          $tpl->set_content($template);
-          $tpl->set_type($menu_template_type);
-          $tpl->set_type_dflt(TRUE);
-          $tpl->add_design($simplex);
-          $tpl->save();
-      }
-  }
-  catch( CmsException $e ) {
-      // if we got here, it's prolly because default content was not installed.
-      audit('',$this->GetName(),'Installation Error: '.$e->GetMessage());
-  }
-
+        $fn = cms_join_path(dirname(__FILE__),'templates','Simplex_Footer_Navigation.tpl');
+        if( file_exists( $fn ) ) {
+            $template = @file_get_contents($fn);
+            $tpl = new CmsLayoutTemplate();
+            $tpl->set_name('Simplex Footer Navigation');
+            $tpl->set_owner($uid);
+            $tpl->set_content($template);
+            $tpl->set_type($menu_template_type);
+            $tpl->set_type_dflt(TRUE);
+            $tpl->add_design($simplex);
+            $tpl->save();
+        }
+    }
+    catch( CmsException $e ) {
+        // if we got here, it's prolly because default content was not installed.
+        audit('',$this->GetName(),'Installation Error: '.$e->GetMessage());
+    }
 }
 catch( CmsException $e ) {
   debug_to_log(__FILE__.':'.__LINE__.' '.$e->GetMessage());
