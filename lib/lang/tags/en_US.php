aa01482dd0a0e965b56220123750fd5e002442c6
calguy1000 <calguy1000_old@cmsmadesimple.org>
2015-06-09 16:53:28 +0000

    update help for {cms_jquery} plugin.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@10033 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/lang/tags/en_US.php
+++ lib/lang/tags/en_US.php
@@ -987,7 +987,7 @@ $lang['help_function_cms_jquery'] = <<<EOT
 <p>Simply insert this tag into your page or template: <code>{cms_jquery}</code></p>
 
 <h3>Sample</h3>
-<pre><code>{cms_jquery cdn='true' exclude='jquery.ui.nestedSortable.js' append='uploads/NCleanBlue/js/ie6fix.js' include_css=0}</code></pre>
+<pre><code>{cms_jquery cdn='true' exclude='jquery-ui' append='uploads/NCleanBlue/js/ie6fix.js' include_css=0}</code></pre>
 <h4><em>Outputs</em></h4>
 <pre><code>&lt;script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"&gt;&lt;/script&gt;
 &lt;script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"&gt;&lt;/script&gt;
@@ -995,17 +995,18 @@ $lang['help_function_cms_jquery'] = <<<EOT
 &lt;script type="text/javascript" src="uploads/NCleanBlue/js/ie6fix.js"&gt;&lt;/script&gt;
 </code></pre>
 
-<h3><em>Included Defaults</em></h3>
+<h3>>Known Scripts:</h3>
 <ul>
-	<li><tt>jQuery</tt><em>(1.7.2)</em> - jquery.min.js</li>
-	<li><tt>jQuery UI</tt><em>(1.8.21)</em> - jquery-ui.min.js</li>
-	<li><tt>nestedSortable</tt>(1.3.4) - jquery.ui.nestedSortable.js</li>
-	<li><tt>jQuery json</tt><em>(2.3)</em> - jquery.json.min.js</li>
+	<li><tt>jQuery</tt><em>(1.11.1)</em></li>
+	<li><tt>jQuery-UI</tt><em>(1.8.21)</em></li>
+	<li><tt>nestedSortable</tt><em>(1.3.4)</em></li>
+	<li><tt>json</tt><em>(2.3)</em></li>
+	<li><tt>migrate</tt><em>(2.3)</em></li>
 </ul>
 
 <h3>What parameters does it take?</h3>
 <ul>
-	<li><em>(optional) </em><tt>exclude</tt> - use comma seperated value(CSV) list of scripts you would like to exclude. <code>'jquery.ui.nestedSortable.js,jquery.json.min.js'</code></li>
+	<li><em>(optional) </em><tt>exclude</tt> - use comma seperated value(CSV) list of scripts you would like to exclude. <code>'jquery-ui,migrate'</code></li>
 	<li><em>(optional) </em><tt>append</tt> - use comma seperated value(CSV) list of script paths you would like to append. <code>'/uploads/jquery.ui.nestedSortable.js,http://code.jquery.com/jquery-1.7.1.min.js'</code></li>
 	<li><em>(optional) </em><tt>cdn</tt> - cdn='true' will insert jQuery and jQueryUI Frameworks using Google's Content Delivery Netwok. Default is false.</li>
 	<li><em>(optional) </em><tt>ssl</tt> - use to use the ssl_url as the base path.</li>
