af20887f1ddc3dbbdee88f8b5b9c258e649f9b17
calguy1000 <calguy1000_old@cmsmadesimple.org>
2015-06-26 01:32:06 +0000

    minor.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@10058 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/classes/internal/class.CMSSmartySecurityPolicy.php
+++ lib/classes/internal/class.CMSSmartySecurityPolicy.php
@@ -37,7 +37,7 @@ final class CMSSmartySecurityPolicy extends Smarty_Security
 
     public $secure_dir = null; // this is the magic that stops stuff from happening outside of the specified directories.
     public $php_modifiers = array();
-    //public $php_modifiers = array('escape','count','preg_replace','lang', 'ucwords','print_r','var_dump','trim','htmlspecialchars','explode','htmlspecialchars_decode','strpos','strrpos','startswith','endswith');
+    //public $php_modifiers = array('escape','count','preg_replace','lang', 'ucwords','print_r','var_dump','trim','htmlspecialchars','explode','htmlspecialchars_decode','strpos','strrpos','startswith','endswith','substr);
     public $streams = null;
     public $allow_constants = false;
     //public $allow_super_globals = false;
@@ -57,7 +57,7 @@ final class CMSSmartySecurityPolicy extends Smarty_Security
                 // this should allow most stuff that does modification to data or formatting.
                 // i.e: string searches, array searches, string comparison, sorting, etc.
                 $this->php_functions = array('isset', 'implode','explode','empty','count', 'sizeof','in_array', 'is_array','time','lang',
-                                             'is_string','strpos','strtolower','strtoupper','strcmp','strcasecmp','strlen','array_search','sort','asort',
+                                             'is_string','strpos','substr','strtolower','strtoupper','strcmp','strcasecmp','strlen','array_search','sort','asort',
                                              'nl2br','file_exists', 'is_object', 'is_file','is_dir','print_r','var_dump',
                                              'debug_display','debug_to_log',
                                              'htmlspecialchars','htmlspecialchars_decode','cms_html_entity_decode');
