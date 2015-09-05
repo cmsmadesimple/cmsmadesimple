aae0534d4bad12369c2d6cf6ec8c087f6ba3410e
calguy1000 <calguy1000_old@cmsmadesimple.org>
2015-09-05 14:27:50 +0000

    Fix upgrade routines for News and Search (exception handling, and fix template names).
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@10144 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/News/method.upgrade.php
+++ modules/News/method.upgrade.php
@@ -10,23 +10,45 @@ if( cmsms()->test_state(CmsApp::STATE_INSTALL) ) {
 }
 
 if( version_compare($oldversion,'2.50') < 0 ) {
-  // create template types.
-  $upgrade_template = function($type,$prefix,$tplname,$currentdflt,$prefix2) use (&$mod,$uid) {
-      if( !startswith($tplname,$prefix) ) return;
-      $contents = $mod->GetTemplate($tplname);
-      if( !$contents ) return;
-      $prototype = substr($tplname,strlen($prefix));
+    $_fix_name = function($str) {
+        if( CmsAdminUtils::is_valid_itemname($str) ) return $str;
+        $orig = $str;
+        $str = trim($str);
+        if( !CmsAdminUtils::is_valid_itemname($str[0]) ) $str[0] = '_';
+        for( $i = 1; $i < strlen($str); $i++ ) {
+            if( !CmsAdminUtils::is_valid_itemname($str[$i]) ) $str[$i] = '_';
+        }
+        for( $i = 0; $i < 5; $i++ ) {
+            $in = $str;
+            $str = str_replace('__','_',$str);
+            if( $in == $str ) break;
+        }
+        if( $str == '_' ) throw new \Exception('Invalid name '.$orig.' and cannot be corrected');
+        return $str;
+    };
 
-      $tpl = new CmsLayoutTemplate();
-      $tpl->set_name($tpl::generate_unique_name($prototype,$prefix2));
-      $tpl->set_owner($uid);
-      $tpl->set_content($contents);
-      $tpl->set_type($type);
-      $tpl->set_type_dflt($prototype == $mod->GetPreference($currentdflt));
+    // create template types.
+    $upgrade_template = function($type,$prefix,$tplname,$currentdflt,$prefix2) use (&$mod,&$_fix_name,$uid) {
+        if( !startswith($tplname,$prefix) ) return;
+        $contents = $mod->GetTemplate($tplname);
+        if( !$contents ) return;
+        $prototype = substr($tplname,strlen($prefix));
+        $prototype = $_fix_name($prototype);
 
-      $tpl->save();
+        try {
+            $tpl = new CmsLayoutTemplate();
+            $tpl->set_name($tpl::generate_unique_name($prototype,$prefix2));
+            $tpl->set_owner($uid);
+            $tpl->set_content($contents);
+            $tpl->set_type($type);
+            $tpl->set_type_dflt($prototype == $mod->GetPreference($currentdflt));
+            $tpl->save();
 
-      $mod->DeleteTemplate($tplname);
+            $mod->DeleteTemplate($tplname);
+        }
+        catch( \CmsInvalidDataException $e ) {
+        }
+
   };
 
   try {
@@ -55,54 +77,73 @@ if( version_compare($oldversion,'2.50') < 0 ) {
       $mod = $this;
       $alltemplates = $this->ListTemplates();
 
-    $summary_template_type = new CmsLayoutTemplateType();
-    $summary_template_type->set_originator($this->GetName());
-    $summary_template_type->set_name('summary');
-    $summary_template_type->set_dflt_flag(TRUE);
-    $summary_template_type->set_lang_callback('News::page_type_lang_callback');
-    $summary_template_type->set_content_callback('News::reset_page_type_defaults');
-    $summary_template_type->reset_content_to_factory();
-    $summary_template_type->save();
-    foreach( $alltemplates as $tplname ) {
-      $upgrade_template($summary_template_type,'summary',$tplname,'current_summary_template','News-Summary-');
-    }
+      try {
+          $summary_template_type = new CmsLayoutTemplateType();
+          $summary_template_type->set_originator($this->GetName());
+          $summary_template_type->set_name('summary');
+          $summary_template_type->set_dflt_flag(TRUE);
+          $summary_template_type->set_lang_callback('News::page_type_lang_callback');
+          $summary_template_type->set_content_callback('News::reset_page_type_defaults');
+          $summary_template_type->reset_content_to_factory();
+          $summary_template_type->save();
+          foreach( $alltemplates as $tplname ) {
+              $upgrade_template($summary_template_type,'summary',$tplname,'current_summary_template','News-Summary-');
+          }
+      }
+      catch( \CmsInvalidDataException $e ) {
+          // ignore this error.
+      }
 
-    $detail_template_type = new CmsLayoutTemplateType();
-    $detail_template_type->set_originator($this->GetName());
-    $detail_template_type->set_name('detail');
-    $detail_template_type->set_dflt_flag(TRUE);
-    $detail_template_type->set_lang_callback('News::page_type_lang_callback');
-    $detail_template_type->set_content_callback('News::reset_page_type_defaults');
-    $detail_template_type->reset_content_to_factory();
-    $detail_template_type->save();
-    foreach( $alltemplates as $tplname ) {
-      $upgrade_template($detail_template_type,'detail',$tplname,'current_detail_template','News-Detail-');
-    }
+      try {
+          $detail_template_type = new CmsLayoutTemplateType();
+          $detail_template_type->set_originator($this->GetName());
+          $detail_template_type->set_name('detail');
+          $detail_template_type->set_dflt_flag(TRUE);
+          $detail_template_type->set_lang_callback('News::page_type_lang_callback');
+          $detail_template_type->set_content_callback('News::reset_page_type_defaults');
+          $detail_template_type->reset_content_to_factory();
+          $detail_template_type->save();
+          foreach( $alltemplates as $tplname ) {
+              $upgrade_template($detail_template_type,'detail',$tplname,'current_detail_template','News-Detail-');
+          }
+      }
+      catch( \CmsInvalidDataException $e ) {
+          // ignore this error.
+      }
 
-    // Setup form template
-    $form_template_type = new CmsLayoutTemplateType();
-    $form_template_type->set_originator($this->GetName());
-    $form_template_type->set_name('form');
-    $form_template_type->set_dflt_flag(TRUE);
-    $form_template_type->set_lang_callback('News::page_type_lang_callback');
-    $form_template_type->set_content_callback('News::reset_page_type_defaults');
-    $form_template_type->reset_content_to_factory();
-    $form_template_type->save();
-    foreach( $alltemplates as $tplname ) {
-      $upgrade_template($form_template_type,'form',$tplname,'current_form_template','News-Form-');
-    }
+      try {
+          $form_template_type = new CmsLayoutTemplateType();
+          $form_template_type->set_originator($this->GetName());
+          $form_template_type->set_name('form');
+          $form_template_type->set_dflt_flag(TRUE);
+          $form_template_type->set_lang_callback('News::page_type_lang_callback');
+          $form_template_type->set_content_callback('News::reset_page_type_defaults');
+          $form_template_type->reset_content_to_factory();
+          $form_template_type->save();
+          foreach( $alltemplates as $tplname ) {
+              $upgrade_template($form_template_type,'form',$tplname,'current_form_template','News-Form-');
+          }
+      }
+      catch( \CmsInvalidDataException $e ) {
+          // ignore this error.
+      }
 
-    $browsecat_template_type = new CmsLayoutTemplateType();
-    $browsecat_template_type->set_originator($this->GetName());
-    $browsecat_template_type->set_name('browsecat');
-    $browsecat_template_type->set_dflt_flag(TRUE);
-    $browsecat_template_type->set_lang_callback('News::page_type_lang_callback');
-    $browsecat_template_type->set_content_callback('News::reset_page_type_defaults');
-    $browsecat_template_type->reset_content_to_factory();
-    $browsecat_template_type->save();
-    foreach( $alltemplates as $tplname ) {
-      $upgrade_template($browsecat_template_type,'browsecat',$tplname,'current_browsecat_template','News-Browsecat-');
-    }
+      try {
+          $browsecat_template_type = new CmsLayoutTemplateType();
+          $browsecat_template_type->set_originator($this->GetName());
+          $browsecat_template_type->set_name('browsecat');
+          $browsecat_template_type->set_dflt_flag(TRUE);
+          $browsecat_template_type->set_lang_callback('News::page_type_lang_callback');
+          $browsecat_template_type->set_content_callback('News::reset_page_type_defaults');
+          $browsecat_template_type->reset_content_to_factory();
+          $browsecat_template_type->save();
+          foreach( $alltemplates as $tplname ) {
+              $upgrade_template($browsecat_template_type,'browsecat',$tplname,'current_browsecat_template','News-Browsecat-');
+          }
+      }
+      catch( \CmsInvalidDataException $e ) {
+          // ignore this error.
+      }
   }
   catch( CmsException $e ) {
     audit('',$this->GetName(),'Upgrade Error: '.$e->GetMessage());
