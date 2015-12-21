cc42005f5177563d25555f277f9d61a2dad1afdb
calguy1000 <calguy1000_old@cmsmadesimple.org>
2015-12-21 17:36:30 +0000

    Fix so that MenuManager cleans up properly on uninstall.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@10355 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/MenuManager/method.uninstall.php
+++ modules/MenuManager/method.uninstall.php
@@ -1,12 +1,25 @@
 <?php
 
-// drop preferences
 $this->RemovePreference();
-
-// drop templates
-$this->DeleteTemplate();
-
-// remove plugin handlers
+$this->DeleteTemplate(); // old templates
 $this->RemoveSmartyPlugin();
+
+try {
+    $types = CmsLayoutTemplateType::load_all_by_originator($this->GetName());
+    foreach( $types as $type ) {
+        $templates = $type->get_template_list();
+        if( is_array($templates) && count($templates) ) {
+            foreach( $templates as $tpl ) {
+                $tpl->delete();
+            }
+        }
+        $type->delete();
+    }
+}
+catch( CmsException $e ) {
+    // log it
+    audit('',$this->GetName(),'Uninstall Error: '.$e->GetMessage());
+    return FALSE;
+}
 
 ?>
