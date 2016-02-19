150d7fe4fdae7af02daec333249bc01454d9f19e
calguy1000 <calguy1000_old@cmsmadesimple.org>
2016-02-19 00:17:41 +0000

    minor change to the {admin_icon} plugin.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/cms2_no_adodb@10458 3d254a34-79dc-0310-9e5f-be208747d8a0

--- admin/plugins/function.admin_icon.php
+++ admin/plugins/function.admin_icon.php
@@ -18,50 +18,50 @@
 
 function smarty_function_admin_icon($params,&$template)
 {
-  $smarty = $template->smarty;
+    $smarty = $template->smarty;
 
-  if( !cmsms()->test_state(CmsApp::STATE_ADMIN_PAGE) ) return;
+    if( !cmsms()->test_state(CmsApp::STATE_ADMIN_PAGE) ) return;
 
-  $icon = null;
-  $tagparms = array();
-  foreach( $params as $key => $value ) {
-    switch( $key ) {
-    case 'icon':
-      $icon = trim($value);
-      break;
-    case 'width':
-    case 'height':
-    case 'alt':
-    case 'rel':
-    case 'class':
-    case 'id':
-    case 'name':
-    case 'title':
-    case 'accesskey':
-      $tagparms[$key] = trim($value);
-      break;
-    case 'assign':
-      break;
+    $icon = null;
+    $tagparms = array('class'=>'systemicon');
+    foreach( $params as $key => $value ) {
+        switch( $key ) {
+        case 'icon':
+            $icon = trim($value);
+            break;
+        case 'width':
+        case 'height':
+        case 'alt':
+        case 'rel':
+        case 'class':
+        case 'id':
+        case 'name':
+        case 'title':
+        case 'accesskey':
+            $tagparms[$key] = trim($value);
+            break;
+        case 'assign':
+            break;
+        }
     }
-  }
 
-  if( !$icon ) return;
-  $fnd = cms_admin_utils::get_icon($icon);
-  if( !$fnd ) return;
+    if( !$icon ) return;
+    $fnd = cms_admin_utils::get_icon($icon);
+    if( !$fnd ) return;
 
-  if( !isset($tagparms['alt']) ) $tagparms['alt'] = basename($fnd);
+    if( !isset($tagparms['alt']) ) $tagparms['alt'] = basename($fnd);
 
-  $out = "<img src=\"{$fnd}\"";
-  foreach( $tagparms as $key => $value ) {
-    $out .= " $key=\"$value\"";
-  }
-  $out .= '/>';
+    $out = "<img src=\"{$fnd}\"";
+    foreach( $tagparms as $key => $value ) {
+        $out .= " $key=\"$value\"";
+    }
+    $out .= '/>';
 
-  if( isset($params['assign']) ) {
-    $smarty->assign(trim($params['assign']),$out);
-    return;
-  }
-  return $out;
+    if( isset($params['assign']) ) {
+        $smarty->assign(trim($params['assign']),$out);
+        return;
+    }
+    return $out;
 }
 
 ?>
