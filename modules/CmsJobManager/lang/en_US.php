ef19133a88ae4ce9bb26574aded47fb7b9090f47
calguy1000 <calguy1000_old@cmsmadesimple.org>
2016-05-10 23:11:34 +0000

    lang strings.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/cms2_no_adodb@10623 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/CmsJobManager/lang/en_US.php
+++ modules/CmsJobManager/lang/en_US.php
@@ -1,4 +1,9 @@
 <?php
+$lang['evtdesc_OnJobFailed'] = 'An event that is triggered when a job is removed from the job queue after failing too many times';
+$lang['evthelp_OnJobFailed'] = '<h4>Parameters:</h4>
+<ul>
+  <li>"job" - A reference to the \CMSMS\Async\Job job object that has failed</li>
+</ul';
 $lang['info_background_jobs'] = 'This panel lists information about all currently known background jobs.  It is normal for jobs to appear and disappear on this list frequently.  If a jub has a high error count OR never started it may mean that you need to investigate the reasons for that error.';
 $lang['until'] = 'Until';
 $lang['info_no_jobs'] = 'There are currently no jobs in the queue';
