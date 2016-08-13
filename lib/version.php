b0fe156722fd510ade4ba4efd268960104d3c454
calguy1000 <calguy1000_old@cmsmadesimple.org>
2016-08-13 13:29:39 +0000

    assign the name 'High Rock'
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@10755 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -20,7 +20,7 @@
 #$Id: version.php 10177 2015-09-11 15:02:02Z calguy1000 $
 
 $CMS_VERSION = "2.1.5";
-$CMS_VERSION_NAME = "NO NAME SET";
+$CMS_VERSION_NAME = "High Rock";
 $CMS_SCHEMA_VERSION = '201';
 
 define('CMS_VERSION', $CMS_VERSION);
