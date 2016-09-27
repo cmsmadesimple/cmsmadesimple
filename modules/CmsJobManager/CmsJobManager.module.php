63e1b604901daffebd60480d981f0a1839872d73
calguy1000 <calguy1000_old@cmsmadesimple.org>
2016-09-27 16:14:06 +0000

    minor comment.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/cms2_no_adodb@10851 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/CmsJobManager/CmsJobManager.module.php
+++ modules/CmsJobManager/CmsJobManager.module.php
@@ -1,27 +1,15 @@
 <?php
 #BEGIN_LICENSE
 #-------------------------------------------------------------------------
-# Module: CmsAsyncJobManager (c) 2016 by Robert Campbell
+# Module: CmsJobManager (c) 2016 by Robert Campbell
 #         (calguy1000@cmsmadesimple.org)
-#  An addon module for CMS Made Simple to allow management of asynchronous jobs
+#  A core module for CMS Made Simple to allow management of asynchronous jobs
 #  and cron jobs.
-#
-#-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2005 by Ted Kulp (wishy@cmsmadesimple.org)
-# Visit the CMSMS Homepage at: http://www.cmsmadesimple.org
-#
-#-------------------------------------------------------------------------
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin
-# section that the site was built with CMS Made simple.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
