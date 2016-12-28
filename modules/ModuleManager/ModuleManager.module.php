47e713cc366ef241070dc3994c7770e21c025e4b
calguy1000 <calguy1000_old@cmsmadesimple.org>
2016-12-28 00:38:05 +0000

    now use https.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@10938 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/ModuleManager/ModuleManager.module.php
+++ modules/ModuleManager/ModuleManager.module.php
@@ -40,11 +40,11 @@ define('MINIMUM_REPOSITORY_VERSION','1.5');
 
 class ModuleManager extends CMSModule
 {
-  const _dflt_request_url = 'http://www.cmsmadesimple.org/ModuleRepository/request/v2/';
+  const _dflt_request_url = 'https://www.cmsmadesimple.org/ModuleRepository/request/v2/';
 
   function GetName() { return get_class($this); }
   function GetFriendlyName() { return $this->Lang('friendlyname'); }
-  function GetVersion() { return '2.0.5'; }
+  function GetVersion() { return '2.0.5.1'; }
   function GetHelp() { return $this->Lang('help'); }
   function GetAuthor() { return 'calguy1000'; }
   function GetAuthorEmail() { return 'calguy1000@hotmail.com'; }
