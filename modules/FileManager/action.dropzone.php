cf1f83f09091e6af1c348048e7f0dcc3f6302a40
calguy1000 <calguy1000_old@cmsmadesimple.org>
2017-01-01 21:01:47 +0000

    minor.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/cms2_no_adodb@10966 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/FileManager/action.dropzone.php
+++ modules/FileManager/action.dropzone.php
@@ -22,40 +22,39 @@ if( strlen($advancedmode) > 1 ) $advancedmode = 0;
     $smarty->assign('cwd',$cwd);
 
     $startdir = $config['uploads_path'];
-    if( $this->AdvancedAccessAllowed() && $advancedmode ) {
-        $startdir = $config['root_path'];
+    if( $this->AdvancedAccessAllowed() && $advancedmode ) $startdir = $config['root_path'];
+
+    // now get a simple list of all of the directories we have 'write' access to.
+    $basedir = dirname($startdir);
+    function get_dirs($startdir,$prefix = '/') {
+        $res = array();
+        if( !is_dir($startdir) ) return;
+
+        global $showhiddenfiles;
+        $dh = opendir($startdir);
+        while( false !== ($entry = readdir($dh)) ) {
+            if( $entry == '.' ) continue;
+            if( $entry == '..' ) continue;
+            $full = filemanager_utils::join_path($startdir,$entry);
+            if( !is_dir($full) ) continue;
+            if( !is_readable($full) ) continue;
+            if( !$showhiddenfiles && ($entry[0] == '.' || $entry[0] == '_') ) continue;
+
+            if( $entry == '.svn' || $entry == '.git' ) continue;
+            if( is_writable($full) ) $res[$prefix.$entry] = $prefix.$entry;
+            $tmp = get_dirs($full,$prefix.$entry.'/');
+            if( is_array($tmp) && count($tmp) ) $res = array_merge($res,$tmp);
+        }
+        closedir($dh);
+        return $res;
     }
 
-  // now get a simple list of all of the directories we have 'write' access to.
-  $basedir = dirname($startdir);
-  function get_dirs($startdir,$prefix = '/') {
-      $res = array();
-      if( !is_dir($startdir) ) return;
-
-      global $showhiddenfiles;
-      $dh = opendir($startdir);
-      while( false !== ($entry = readdir($dh)) ) {
-          if( $entry == '.' ) continue;
-          if( $entry == '..' ) continue;
-          $full = filemanager_utils::join_path($startdir,$entry);
-          if( !is_dir($full) ) continue;
-          if( !$showhiddenfiles && ($entry[0] == '.' || $entry[0] == '_') ) continue;
-
-          if( $entry == '.svn' || $entry == '.git' ) continue;
-          $res[$prefix.$entry] = $prefix.$entry;
-          $tmp = get_dirs($full,$prefix.$entry.'/');
-          if( is_array($tmp) && count($tmp) ) $res = array_merge($res,$tmp);
-      }
-      closedir($dh);
-      return $res;
-  }
-
-  $output = get_dirs($startdir,'/'.basename($startdir).'/');
-  $output['/'.basename($startdir)] = '/'.basename($startdir);
-  if( count($output) ) {
-      ksort($output);
-      $smarty->assign('dirlist',$output);
-  }
+    $output = get_dirs($startdir,'/'.basename($startdir).'/');
+    $output['/'.basename($startdir)] = '/'.basename($startdir);
+    if( count($output) ) {
+        ksort($output);
+        $smarty->assign('dirlist',$output);
+    }
 }
 
 $smarty->assign('FileManager',$this);
@@ -65,5 +64,3 @@ if( isset($params['template']) ) {
     if( !endswith($template,'.tp;') )  $template .= '.tpl';
 }
 echo $this->ProcessTemplate($template);
-
-?>
