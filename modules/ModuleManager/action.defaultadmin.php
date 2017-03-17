7f9f572d5378f582d6379ca05e71378435e7f54f
calguy1000 <calguy1000_old@cmsmadesimple.org>
2017-03-17 15:17:27 +0000

    minor cleanup.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@11173 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/ModuleManager/action.defaultadmin.php
+++ modules/ModuleManager/action.defaultadmin.php
@@ -1,11 +1,11 @@
 <?php
 #BEGIN_LICENSE
 #-------------------------------------------------------------------------
-# Module: ModuleManager (c) 2008 by Robert Campbell 
+# Module: ModuleManager (c) 2008 by Robert Campbell
 #         (calguy1000@cmsmadesimple.org)
 #  An addon module for CMS Made Simple to allow browsing remotely stored
 #  modules, viewing information about them, and downloading or upgrading
-# 
+#
 #-------------------------------------------------------------------------
 # CMS - CMS Made Simple is (c) 2005 by Ted Kulp (wishy@cmsmadesimple.org)
 # Visit our homepage at: http://www.cmsmadesimple.org
@@ -20,7 +20,7 @@
 # However, as a special exception to the GPL, this software is distributed
 # as an addon module to CMS Made Simple.  You may not use this software
 # in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin 
+# Made simple that does not indicate clearly and obviously in its admin
 # section that the site was built with CMS Made simple.
 #
 # This program is distributed in the hope that it will be useful,
@@ -37,24 +37,26 @@
 if( !isset($gCms) ) exit;
 
 if( isset($params['modulehelp']) ) {
-  $params['mod'] = $params['modulehelp'];
-  unset($params['modulehelp']);
-  include(__DIR__.'/action.local_help.php');
-  return;
+    // this is done before permissions checks
+    $params['mod'] = $params['modulehelp'];
+    unset($params['modulehelp']);
+    include(__DIR__.'/action.local_help.php');
+    return;
 }
 
+if( !$this->VisibleToAdminUser() ) exit;
 $tmp = ModuleOperations::get_instance()->GetQueueResults();
 if( is_array($tmp) && count($tmp) ) {
-  $tmp2 = array();
-  foreach( $tmp as $key => $data ) {
-    $msg = $data[1];
-    if( !$msg ) {
-      $msg = $this->Lang('unknown');
-      if( $data[0] ) $msg = $this->Lang('success');
+    $tmp2 = array();
+    foreach( $tmp as $key => $data ) {
+        $msg = $data[1];
+        if( !$msg ) {
+            $msg = $this->Lang('unknown');
+            if( $data[0] ) $msg = $this->Lang('success');
+        }
+        $tmp2[] = $key.': '.$msg;
     }
-    $tmp2[] = $key.': '.$msg;
-  }
-  echo $this->ShowMessage($tmp2);
+    echo $this->ShowMessage($tmp2);
 }
 
 echo '<div class="pagewarning">'."\n";
@@ -65,60 +67,56 @@ echo '<h3>'.$this->Lang('use_at_your_own_risk')."</h3>\n";
 echo '<p>'.$this->Lang('compatibility_disclaimer')."</p></div>\n";
 
 $connection_ok = modmgr_utils::is_connection_ok();
-if( !$connection_ok ) {
-    echo $this->ShowErrors($this->Lang('error_request_problem'));
-}
+if( !$connection_ok ) echo $this->ShowErrors($this->Lang('error_request_problem'));
 
 // this is a bit ugly.
 modmgr_utils::get_images();
 
 $newversions = null;
 if( $connection_ok ) {
-try {
-  $newversions = modulerep_client::get_newmoduleversions();
-}
-catch( Exception $e ) {
-  echo $this->ShowErrors($e->GetMessage());
-}
+    try {
+        $newversions = modulerep_client::get_newmoduleversions();
+    }
+    catch( Exception $e ) {
+        echo $this->ShowErrors($e->GetMessage());
+    }
 }
 
 echo $this->StartTabHeaders();
 if( $this->CheckPermission('Modify Modules') ) {
-  echo $this->SetTabHeader('installed',$this->Lang('installed'));
-  if( $connection_ok ) { 
-	echo $this->SetTabHeader('newversions',count($newversions).' '.$this->Lang('tab_newversions') );
-    echo $this->SetTabHeader('search',$this->Lang('search'));
-    echo $this->SetTabHeader('modules',$this->Lang('availmodules'));
-  }
+    echo $this->SetTabHeader('installed',$this->Lang('installed'));
+    if( $connection_ok ) {
+        echo $this->SetTabHeader('newversions',count($newversions).' '.$this->Lang('tab_newversions') );
+        echo $this->SetTabHeader('search',$this->Lang('search'));
+        echo $this->SetTabHeader('modules',$this->Lang('availmodules'));
+    }
 }
 if( $this->CheckPermission('Modify Site Preferences') ) echo $this->SetTabHeader('prefs',$this->Lang('prompt_settings'));
 echo $this->EndTabHeaders();
 
 echo $this->StartTabContent();
 if( $this->CheckPermission('Modify Modules') ) {
-  echo $this->StartTab('installed',$params);
-  include(dirname(__FILE__).'/function.admin_installed.php');
-  echo $this->EndTab();
-
-  if( $connection_ok ) {
-	echo $this->StartTab('newversions',$params);
-	include(dirname(__FILE__).'/function.newversionstab.php');
-	echo $this->EndTab();
-
-    echo $this->StartTab('search',$params);
-    include(dirname(__FILE__).'/function.search.php');
+    echo $this->StartTab('installed',$params);
+    include(dirname(__FILE__).'/function.admin_installed.php');
     echo $this->EndTab();
 
-    echo $this->StartTab('modules',$params);
-    include(dirname(__FILE__).'/function.admin_modules_tab.php');
-    echo $this->EndTab();
-  }
+    if( $connection_ok ) {
+        echo $this->StartTab('newversions',$params);
+        include(dirname(__FILE__).'/function.newversionstab.php');
+        echo $this->EndTab();
+
+        echo $this->StartTab('search',$params);
+        include(dirname(__FILE__).'/function.search.php');
+        echo $this->EndTab();
+
+        echo $this->StartTab('modules',$params);
+        include(dirname(__FILE__).'/function.admin_modules_tab.php');
+        echo $this->EndTab();
+    }
 }
 if( $this->CheckPermission('Modify Site Preferences') ) {
-  echo $this->StartTab('prefs',$params);
-  include(dirname(__FILE__).'/function.admin_prefs_tab.php');
-  echo $this->EndTab();
+    echo $this->StartTab('prefs',$params);
+    include(dirname(__FILE__).'/function.admin_prefs_tab.php');
+    echo $this->EndTab();
 }
 echo $this->EndTabContent();
-
-?>
--- modules/ModuleManager/action.local_help.php
+++ modules/ModuleManager/action.local_help.php
@@ -3,8 +3,8 @@ if( !isset($gCms) ) exit;
 //if( !$this->CheckPermission('Modify Modules') ) return;
 $this->SetCurrentTab('installed');
 if( !isset($params['mod']) ) {
-  $this->SetError($this->Lang('error_missingparam'));
-  $this->RedirectToAdminTab();
+    $this->SetError($this->Lang('error_missingparam'));
+    $this->RedirectToAdminTab();
 }
 $module = get_parameter_value($params,'mod');
 $lang = get_parameter_value($params,'lang');
@@ -13,28 +13,27 @@ $lang = get_parameter_value($params,'lang');
 $ops = ModuleOperations::get_instance();
 $modinstance = $ops->get_module_instance($module,'',TRUE);
 if( !is_object($modinstance) ) {
-  $this->SetError($this->Lang('error_getmodule',$module));
-  $this->RedirectToAdminTab();
+    $this->SetError($this->Lang('error_getmodule',$module));
+    $this->RedirectToAdminTab();
 }
-
 $theme = cms_utils::get_theme_object();
 $theme->SetTitle('module_help');
 
-$our_lang =  CmsNlsOperations::get_current_language();
+$our_lang = CmsNlsOperations::get_current_language();
 $smarty->assign('our_lang',$our_lang);
 
 if( $our_lang != 'en_US' ) {
-  if( $lang != '' ) {
-    $smarty->assign('mylang_text',$this->Lang('display_in_mylanguage'));
-    $smarty->assign('mylang_url',$this->create_url($id,'local_help',$returnid,array('mod'=>$module)));
-    CmsNlsOperations::set_language('en_US');
-  }
-  else {
-    $yourlang_url = $this->create_url($id,'local_help',$returnid,array('mod'=>$module,'lang'=>'en_US'));
-    $smarty->assign('our_lang',$our_lang);
-    $smarty->assign('englang_url',$yourlang_url);
-    $smarty->assign('englang_text',$this->Lang('display_in_english'));
-  }
+    if( $lang != '' ) {
+        $smarty->assign('mylang_text',$this->Lang('display_in_mylanguage'));
+        $smarty->assign('mylang_url',$this->create_url($id,'local_help',$returnid,array('mod'=>$module)));
+        CmsNlsOperations::set_language('en_US');
+    }
+    else {
+        $yourlang_url = $this->create_url($id,'local_help',$returnid,array('mod'=>$module,'lang'=>'en_US'));
+        $smarty->assign('our_lang',$our_lang);
+        $smarty->assign('englang_url',$yourlang_url);
+        $smarty->assign('englang_text',$this->Lang('display_in_english'));
+    }
 }
 
 $smarty->assign('module_name',$modinstance->GetName());
@@ -42,9 +41,7 @@ $smarty->assign('friendly_name',$modinstance->GetFriendlyName());
 
 $smarty->assign('help_page',$modinstance->GetHelpPage());
 if( $our_lang != 'en_US' && $lang != '' ) {
-  CmsNlsOperations::set_language($our_lang);
+    CmsNlsOperations::set_language($our_lang);
 }
 
 echo $this->ProcessTemplate('local_help.tpl');
-
-?>
