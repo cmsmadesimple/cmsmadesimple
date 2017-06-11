c2075f968b03b31643f6b5c2720507afb8916b83
calguy1000 <calguy1000_old@cmsmadesimple.org>
2017-06-11 14:02:25 +0000

    tweaks to upgrade routine for version 2.2.1
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@11263 3d254a34-79dc-0310-9e5f-be208747d8a0

--- phar_installer/app/upgrade/2.2.1/upgrade.php
+++ phar_installer/app/upgrade/2.2.1/upgrade.php
@@ -1,39 +1,11 @@
 <?php
 status_msg('Performing directory changes for CMSMS 2.2.1');
 
-$create_private_dir = function($relative_dir) {
-    $app = \__appbase\get_app();
-    $destdir = $app->get_destdir();
-    $relative_dir = trim($relative_dir);
-    if( !$relative_dir ) return;
-
-    $dir = $destdir.'/'.$relative_dir;
-    if( !is_dir($dir) ) {
-        @mkdir($dir,0777,true);
-    }
-    @touch($dir.'/index.html');
-};
-
-$move_directory_files = function($srcdir,$destdir) {
-    $srcdir = trim($srcdir);
-    $destdir = trim($destdir);
-    if( !is_dir($srcdir) ) return;
-
-    $files = glob($srcdir.'/*');
-    if( !count($files) ) return;
-
-    foreach( $files as $src ) {
-        $bn = basename($src);
-        $dest = $destdir.'/'.$bn;
-        rename($src,$dest);
-    }
-    @touch($dir.'/index.html');
-};
-
 $destdir = \__appbase\get_app()->get_destdir();
 $plugins_from = $destdir.'/plugins';
+if( !is_dir($plugins_from) ) return;
 $plugins_to = $destdir.'/assets/plugins';
-$files = glob($plugins_from.'/*php');
+$files = glob($plugins_from.'/*');
 if( !count($files) ) return;
 
 // check permissions
@@ -41,27 +13,37 @@ if( !is_dir($plugins_to) || !is_writable($plugins_to) ) {
     error_msg('Note: Could not move plugins to /assets/plugins because of permissions in the destination directory');
     return;
 }
-foreach( $fileas as $filespec ) {
-    if( !is_writable( $filespec ) ) {      
+foreach( $files as $filespec ) {
+    if( !is_writable( $filespec ) ) {
         error_msg('Note: Could not move plugins to /assets/plugisn because because of permissions in the source directory');
         return;
     }
-j
+}
+
+$remove = function( $in ) {
+    if( is_file( $in ) ) {
+        @unlink($in);
+    }
+    else if( is_dir( $in ) ) {
+        \__appbase\utils::rrmdir($in);
+    }
+};
 
 // move the files
-foreach( $files as $src_file ) {
+foreach( $files as $src_name ) {
     $bn = basename($src_file);
-    $dest_file = $plugins_to.'/'.$bn;
-    if( ! is_file($bn) ) rename( $src_file, $dest_file );
-    @unlink( $src_file );
+    $dest_name = $plugins_to.'/'.$bn;
+    if( ! is_file($dest_name) && !is_dir($dest_name) ) rename( $src_name, $dest_name );
+    $remove( $src_name );
 }
 
 // maybe remove the directory
 $files = glob($plugins_from.'/*');
-$remove = false;
+$do_remove = false;
 if( count($files) == 0 ) $remove = true;
 if( count($files) == 1 ) {
     $bn = strtolower(basename($files[0]));
-    if( $bn == 'index.html' ) $remove == true;
+    if( $bn == 'index.html' ) $do_remove == true;
 }
-if( $remove ) recursive_remove($plugins_from);
+if( $do_remove ) \__appbase\utils::rrmdir($plugins_from);
+@touch($plugins_to.'/index.html');
