0f11cfaa3bf63f397c29213e92fdda8eaecf8589
calguy1000 <calguy1000_old@cmsmadesimple.org>
2017-06-26 21:24:41 +0000

    Adds a silly __get() method to the NavigatorNode class.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@11328 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/Navigator/Navigator.module.php
+++ modules/Navigator/Navigator.module.php
@@ -35,7 +35,13 @@
 #END_LICENSE
 #$Id: News.module.php 2114 2005-11-04 21:51:13Z wishy $
 
-class NavigatorNode {}
+class NavigatorNode
+{
+    /**
+     * This little function will remove all silly notices in smarty.
+     */
+    public function __get($key) { return null; }
+}
 
 final class Navigator extends CMSModule
 {
@@ -45,7 +51,7 @@ final class Navigator extends CMSModule
     function GetFriendlyName() { return $this->Lang('friendlyname'); }
     function IsPluginModule() { return true; }
     function HasAdmin() { return false; }
-    function GetVersion() { return '1.0.6'; }
+    function GetVersion() { return '1.0.7'; }
     function MinimumCMSVersion() { return '2.1.99'; }
     function GetAdminDescription() { return $this->Lang('description'); }
     function GetAdminSection() { return 'layout'; }
