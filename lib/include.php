17b795e083dcf05f4248a0bf0011d105e9fc12d5
rolf1 <rolf1_old@cmsmadesimple.org>
2017-10-20 14:05:03 +0000

    typo
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@11522 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/include.php
+++ lib/include.php
@@ -201,7 +201,7 @@ if(isset($CMS_ADMIN_PAGE)) CmsNlsOperations::set_language();
 if( !isset($DONT_LOAD_SMARTY) ) {
     debug_buffer('Initialize Smarty');
     $smarty = $_app->GetSmarty();
-    debug_buffer('Done Initialiing Smarty');
+    debug_buffer('Done Initialing Smarty');
     if( defined('CMS_DEBUG') && CMS_DEBUG ) $smarty->error_reporting = 'E_ALL';
     $smarty->assignGlobal('sitename', cms_siteprefs::get('sitename', 'CMSMS Site'));
 }
