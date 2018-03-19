f06d6dac26f877fd1b01820a235601a60aa7a49c
calguy1000 <calguy1000_old@cmsmadesimple.org>
2018-03-19 18:14:18 +0000

    Re-introduces the host_whitelist config entry that got lost in some commit or merge somewhere.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@11610 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -19,8 +19,8 @@
 #
 #$Id: version.php 10177 2015-09-11 15:02:02Z calguy1000 $
 
-$CMS_VERSION = "2.2.7";
-$CMS_VERSION_NAME = "Skookumchuck";
+$CMS_VERSION = "2.2.8";
+$CMS_VERSION_NAME = "NO NAME SET"
 $CMS_SCHEMA_VERSION = '202';
 
 define('CMS_VERSION', $CMS_VERSION);
