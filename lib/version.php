10c8aae51bcbdd12d58c47cc6224c81e1cc60737
calguy1000 <calguy1000_old@cmsmadesimple.org>
2018-06-30 20:04:50 +0000

    Give this thing a name.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@11704 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -20,7 +20,7 @@
 #$Id: version.php 10177 2015-09-11 15:02:02Z calguy1000 $
 
 $CMS_VERSION = "2.2.8";
-$CMS_VERSION_NAME = "NO NAME SET";
+$CMS_VERSION_NAME = "Flin Flon";
 $CMS_SCHEMA_VERSION = '202';
 
 define('CMS_VERSION', $CMS_VERSION);
