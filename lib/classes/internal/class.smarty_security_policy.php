aa4eb8fefc90639acd619d3044f819e80b1a5715
calguy1000 <calguy1000_old@cmsmadesimple.org>
2018-07-14 22:41:03 +0000

    minor code cleanup.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@11722 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/classes/internal/class.smarty_security_policy.php
+++ lib/classes/internal/class.smarty_security_policy.php
@@ -57,11 +57,13 @@ final class smarty_security_policy extends \Smarty_Security
                 $this->static_classes = null;
                 // this should allow most stuff that does modification to data or formatting.
                 // i.e: string searches, array searches, string comparison, sorting, etc.
-                $this->php_functions = array('isset', 'implode','explode','empty','count', 'sizeof','in_array', 'is_array','time','lang',
-                                             'str_replace','is_string','strpos','substr','strtolower','strtoupper','strcmp','strcasecmp','strlen','array_search','sort','ksort','asort',
-                                             'nl2br','file_exists', 'is_object', 'is_file','is_dir','print_r','var_dump', 'array_reverse', 'array_flip','shuffle','array_rand',
-                                             'debug_display','startswith', 'endswith', 'urlencode','json_encode','json_decode',
-                                             'htmlspecialchars','htmlspecialchars_decode','cms_html_entity_decode');
+                $this->php_functions = [
+			'isset', 'implode','explode','empty','count', 'sizeof','in_array', 'is_array','time','lang',
+                        'str_replace','is_string','strpos','substr','strtolower','strtoupper','strcmp','strcasecmp','strlen',
+			'array_search','sort','ksort','asort', 'nl2br','file_exists', 'is_object', 'is_file','is_dir','print_r',
+			'var_dump', 'array_reverse', 'array_flip','shuffle','array_rand', 'debug_display','startswith', 'endswith', 
+			'urlencode','json_encode','json_decode', 'mt_jsbool', 'htmlspecialchars','htmlspecialchars_decode',
+			'cms_html_entity_decode' ];
             }
         }
         else {
