f24dcef642b7f8ff23622b3aad41961844060e89
calguy1000 <calguy1000_old@cmsmadesimple.org>
2018-09-04 21:54:24 +0000

    Fixes to removing modules.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@11753 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/modules/ModuleManager/action.local_remove.php
+++ lib/modules/ModuleManager/action.local_remove.php
@@ -2,23 +2,26 @@
 if( !isset($gCms) ) exit;
 if( !$this->CheckPermission('Modify Modules') ) return;
 $this->SetCurrentTab('installed');
-if( !isset($params['mod']) ) {
-  $this->SetError($this->Lang('error_missingparam'));
-  $this->RedirectToAdminTab();
-}
-$module = get_parameter_value($params,'mod');
 
-$dir = cms_join_path($config['root_path'],'modules',$module);
-$result = recursive_delete( $dir );
+try {
+    if( !isset($params['mod']) ) throw new \LogicException( $this->Lang('error_missingparam') );
+    $module = get_parameter_value($params,'mod');
 
-if( !$result ) {
-  $this->SetError($this->Lang('error_moduleremovefailed'));
+    $dirs = $dir = null;
+    $dirs[] = cms_join_path( CMS_ROOT_PATH, 'lib', 'modules', $module );
+    $dirs[] = cms_join_path( CMS_ASSETS_PATH, 'modules', $module );
+    foreach( $dirs as $dir ) {
+        if( is_dir( $dir ) ) break;
+    }
+    if( !$dir ) throw new \RuntimeException( $this->Lang('error_moduleremovefailed') );
+    if( !is_directory_writable( $dir ) ) throw new \RuntimeException( $this->Lang('error_moduleremovefailed') );
+    $result = recursive_delete( $dir );
+    if( !$result ) throw new \RuntimeException( $this->Lang('error_moduleremovefailed') );
+    $gCms->clear_cached_files();
+    audit('',$this->GetName(),'Module '.$module.' removed');
+    $this->SetMessage($this->Lang('msg_module_removed'));
 }
-else {
-  audit('',$this->GetName(),'Module '.$module.' removed');
-  $this->SetMessage($this->Lang('msg_module_removed'));
+catch( \CmsException $e ) {
+    $this->SetError( $e->GetMessage() );
 }
-
 $this->RedirectToAdminTab();
-
-?>
