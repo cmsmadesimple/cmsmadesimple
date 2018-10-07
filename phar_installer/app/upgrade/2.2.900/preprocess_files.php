61cccc32ff8acbc5285ed5c6818aa274692ae621
calguy1000 <calguy1000_old@cmsmadesimple.org>
2018-10-07 16:50:50 +0000

    Now rename all module folders that are not core modules from /modules to /assets/modules before manifest is processed. Then when manifest is processed, core modules will be destroyed and replaced in /lib/modules.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@11772 3d254a34-79dc-0310-9e5f-be208747d8a0

--- phar_installer/app/upgrade/2.2.900/preprocess_files.php
+++ phar_installer/app/upgrade/2.2.900/preprocess_files.php
@@ -1,5 +1,6 @@
 <?php
 // destination directory is in $destdir
+// NOTE: this script is executed BEFORE manifest operations have taken place.
 
 // in this version we make sure that /assets/modules exists
 // and move /modules/MenuManager to /assets/modules/MenuManager
@@ -8,14 +9,27 @@
 //     then this will cause errors to be reported in the manifest stage, that can be ignored.
 //     use the option "--dnd modules/MenuManager" when creating the manifest to prevent this.
 
-$assets_modules = "$destdir/assets/modules";
-$src_folder = "$destdir/modules/MenuManager";
-$dest_folder = "$assets_modules/MenuManager";
-if( is_dir( $src_folder) && !is_dir($dest_folder) ) {
-    verbose_msg("Moving Menumanger to $assets_modules");
-    if( !is_dir($assets_modules) ) {
-        $res = mkdir( $assets_modules, 0777, true );
-        if( !$res ) throw new \RuntimeException('Problem creating directory at '.$assets_modules);
-    }
-    rename( $src_folder, $dest_folder );
+$dest_dir = "$destdir/assets/modules";
+$src_dir = "$destdir/modules";
+$cmssystemmodules =  [ 'AdminLog', 'AdminSearch', 'DesignManager', 'CMSContentManager', 'FileManager', 'ModuleManager', 'Search','News', 'MicroTiny',
+                       'Navigator', 'CmsJobManager', 'FilePicker', 'CoreAdminLogin' ];
+
+// move any directory that a:  Contains a xxxxx.module.php file,   and b: is not in the above list from /modules to /assets/modules
+
+if( is_dir($dest_dir) ) return;
+$res = mkdir($dest_dir, 0777, true);
+if( !$res ) throw new \RuntimeException('Problem creating directory at '.$assets_modules);
+
+status_msg( "Moving non core modules to assets/modules" );
+$dh = opendir( $src_dir );
+while( ($file = readdir($dh)) !== false ) {
+    if( $file == '.' || $file == '..' ) continue;
+    $full_path = "$src_dir/$file";
+    if( !is_dir( $full_path ) ) continue;
+    if( !is_file( "$full_path/$file.module.php" ) ) continue;
+    if( in_array( $file, $cmssystemmodules ) ) continue;
+
+    verbose_msg("Moving module $file to $dest_dir");
+    $dest_folder = "$dest_dir/$file";
+    rename( $full_path, $dest_folder );
 }
