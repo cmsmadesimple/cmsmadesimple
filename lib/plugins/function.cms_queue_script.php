a76d5524e74e5bc4fdd5c3f4ed610f760ed1fc9b
calguy1000 <calguy1000_old@cmsmadesimple.org>
2019-02-16 23:34:09 +0000

    minor updates and formatting (cuz I haven't entirely figured out phpcs yet).
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@11864 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/plugins/function.cms_queue_script.php
+++ lib/plugins/function.cms_queue_script.php
@@ -2,8 +2,7 @@
 function smarty_function_cms_queue_script( $params, &$template )
 {
     // produces no output.
-    if(!isset($params['file']) ) { return;
-    }
+    if(!isset($params['file']) ) return;
     $combiner = CmsApp::get_instance()->GetScriptManager();
 
     $file = trim($params['file']);
@@ -13,12 +12,10 @@ function smarty_function_cms_queue_script( $params, &$template )
     }
 
     // if it's relative to a CMSMS path
-    if(!startswith($file, DIRECTORY_SEPARATOR) ) { $file = "/$file";
-    }
+    if(!startswith($file, DIRECTORY_SEPARATOR) ) $file = "/$file";
     $config = \cms_config::get_instance();
     $paths = [ CMS_ASSETS_PATH.$file, $config['uploads_path'].$file, CMS_ROOT_PATH.$file ];
     foreach( $paths as $one ) {
-        if(is_file($one) ) { $combiner->queue_script($one);
-        }
+        if(is_file($one) ) $combiner->queue_script($one);
     }
 }
--- lib/plugins/function.cms_render_scripts.php
+++ lib/plugins/function.cms_render_scripts.php
@@ -5,13 +5,11 @@ function smarty_function_cms_render_scripts( $params, $template )
     $force = (isset($params['force'])) ? cms_to_bool($params['force']) : false;
     $nocache = (isset($params['nocache'])) ? cms_to_bool($params['nocache']) : false;
     $defer = (isset($params['defer'])) ? cms_to_bool($params['defer']) : true;
-    if($defer ) { $defer = 'defer';
-    }
+    if($defer ) $defer = 'defer';
 
     $out = null;
     $filename = $combiner->render_scripts(PUBLIC_CACHE_LOCATION, $force);
-    if($nocache ) { $filename .= '?t='.time();
-    }
+    if($nocache ) $filename .= '?t='.time();
 
     if($filename ) {
         $fmt = "<script src=\"%s\" $defer></script>";
