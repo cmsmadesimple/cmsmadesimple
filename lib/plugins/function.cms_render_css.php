ea49ba05fbe41cce91c5032a143a3751fcd527a6
calguy1000 <calguy1000_old@cmsmadesimple.org>
2019-03-20 17:41:24 +0000

    Now uses $config['css_path'] for consistency.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@12024 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/plugins/function.cms_render_css.php
+++ lib/plugins/function.cms_render_css.php
@@ -5,17 +5,19 @@ function smarty_function_cms_render_css( $params, $template )
     if( $sig ) throw new \RuntimeException('cms_render_css can only be called once per request');
     $sig = sha1(__FILE__.time().rand());
     $magic_string = "<!-- cms_render_css:$sig -->";
+    $config = cmsms()->GetConfig();
 
     $force = (isset($params['force'])) ? cms_to_bool($params['force']) : false;
     $nocache = (isset($params['nocache'])) ? cms_to_bool($params['nocache']) : false;
-    $prefix = get_parameter_value($params,'prefix',PUBLIC_CACHE_URL);
+    $prefix = get_parameter_value($params,'prefix',$config['css_url']);
 
-    $on_postrender = function(array $parms) use ($magic_string,$force,$nocache,$prefix) {
+    $on_postrender = function(array $parms) use ($magic_string,$force,$nocache,$prefix,$params,$config) {
         if( !isset($parms['content']) ) return;
         $out = null;
         $combiner = cmsms()->get_stylesheet_manager();
-        $filename = $combiner->render(PUBLIC_CACHE_LOCATION, $force);
-        if($filename ) {
+        $entropy = sha1(__FILE__.json_encode($params));
+        $filename = $combiner->render($config['css_path'], $entropy, $force);
+        if( $filename ) {
             if($nocache ) $filename .= '?t='.time();
             $fmt = "<link rel=\"stylesheet\" href=\"%s\"/>";
             $out = sprintf($fmt, "$prefix/$filename");
