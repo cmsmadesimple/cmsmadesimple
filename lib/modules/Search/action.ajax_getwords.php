32491c144291c583538c9cb69b928ecd9cdd35a8
calguy1000 <calguy1000_old@cmsmadesimple.org>
2019-03-24 17:16:31 +0000

    Throw a 400 error if term is less than 2 characters in length.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@12055 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/modules/Search/action.ajax_getwords.php
+++ lib/modules/Search/action.ajax_getwords.php
@@ -6,20 +6,29 @@ for ($cnt = 0; $cnt < sizeof($handlers); $cnt++) {
     ob_end_clean();
 }
 
-$term = trim(strip_tags(get_parameter_value($_REQUEST,'term')));
-$limit = (int) get_parameter_value($_REQUEST,'limit');
-if( $limit < 1 ) $limit = 10;
-$limit = max(1,min(50,$limit));
+try {
+    $term = trim(strip_tags(get_parameter_value($_REQUEST,'term')));
+    if( strlen($term) < 2) {
+        throw new \RuntimeException('Invalid input');
+    }
+    $limit = (int) get_parameter_value($_REQUEST,'limit');
+    if( $limit < 1 ) $limit = 10;
+    $limit = max(1,min(50,$limit));
 
-if( strpos($term,'%') === FALSE ) $term = "{$term}%";
-$sql = 'SELECT word FROM '.CMS_DB_PREFIX.'module_search_index WHERE word LIKE ? ORDER BY count DESC';
-$dbr = $db->SelectLimit( $sql, $limit, 0, [ $term ] );
+    if( strpos($term,'%') === FALSE ) $term = "{$term}%";
+    $sql = 'SELECT word FROM '.CMS_DB_PREFIX.'module_search_index WHERE word LIKE ? ORDER BY count DESC';
+    $dbr = $db->SelectLimit( $sql, $limit, 0, [ $term ] );
 
-$out = null;
-while( $dbr && !$dbr->EOF() ) {
-    $word = $dbr->fields['word'];
-    $out[] = [ 'label'=>$word, 'value'=>$word ];
-    $dbr->MoveNext();
+    $out = null;
+    while( $dbr && !$dbr->EOF() ) {
+        $word = $dbr->fields['word'];
+        $out[] = [ 'label'=>$word, 'value'=>$word ];
+        $dbr->MoveNext();
+    }
+    if( !empty($out) ) echo json_encode($out);
 }
-if( !empty($out) ) echo json_encode($out);
+catch( \RuntimeException $e ) {
+    header("HTTP/1.0 400 Invalid request");
+    header("Status: 404 Invalid request");
+}
 exit;
