ca84177e54de35e7f677c83108cdc9d829cde5b2
calguy1000 <calguy1000_old@cmsmadesimple.org>
2019-08-18 18:16:46 +0000

    bump version to 2.2.11 - Vulcan so that we can get ready to test.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@12366 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -19,8 +19,8 @@
 #
 #$Id: version.php 10177 2015-09-11 15:02:02Z calguy1000 $
 
-$CMS_VERSION = "2.2.10";
-$CMS_VERSION_NAME = "Spuzzum";
+$CMS_VERSION = "2.2.11";
+$CMS_VERSION_NAME = "Vulcan";
 $CMS_SCHEMA_VERSION = '202';
 
 define('CMS_VERSION', $CMS_VERSION);
