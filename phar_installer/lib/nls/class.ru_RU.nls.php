6fdbd50ea2651a1c17cfe4a03c4e390a9f195b89
rolf1 <rolf1_old@cmsmadesimple.org>
2019-08-22 16:21:38 +0000

    Add Russian nls file to Installation Assistant
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@12381 3d254a34-79dc-0310-9e5f-be208747d8a0

---
+++ phar_installer/lib/nls/class.ru_RU.nls.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace __appbase;
+
+final class ru_RU_nls extends nls
+{
+  public function __construct()
+  {
+    $this->_fullname = 'Russian';
+    $this->_display = 'Русский';
+    $this->_isocode = 'ru';
+    $this->_locale = 'ru_RU';
+    $this->_encoding = 'UTF-8';
+    $this->_aliases = 'ru_RU,ru_RU.cp1251,ru_RU.CP1251,ru_RU.CP866,ru_RU.koi8r,ru_RU.utf8,ru_RU.iso88595,russian,Russian_Russia.1251';
+  }  
+  
+  
+} // end of class
+
+?>
