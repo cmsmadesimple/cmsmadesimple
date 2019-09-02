baa0751b3745b19906a5d3d1238a8ae7a64cbfe2
calguy1000 <calguy1000_old@cmsmadesimple.org>
2019-09-02 19:03:13 +0000

    dammit. syntax error.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/v2.3_dev@12403 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/modules/ModuleManager/lib/class.operations.php
+++ lib/modules/ModuleManager/lib/class.operations.php
@@ -66,126 +66,127 @@ class operations
         $required = array();
         while( $reader->read() ) {
             switch($reader->nodeType) {
-                case \XMLReader::ELEMENT:
-                    switch( strtoupper($reader->localName) ) {
-                        case 'NAME':
-                             $reader->read();
-                             $moduledetails['name'] = $reader->value;
-                             $loaded = $modops->GetLoadedModules();
-                             // check if this module is already installed
-                            if( isset( $loaded[$moduledetails['name']] ) && $overwrite == 0 && $brief == 0 ) {
-                                throw new \CmsLogicException($this->_mod->Lang('err_xml_moduleinstalled'));
-                            }
-                            break;
-
-                        case 'DTDVERSION':
-                             $reader->read();
-                             if( $reader->value != self::MODULE_DTD_VERSION ) throw new \CmsInvalidDataException($this->_mod->Lang('err_xml_dtdmismatch'));
-                             $havedtdversion = true;
-                            break;
-
-                        case 'VERSION':
-                             $reader->read();
-                             $moduledetails['version'] = $reader->value;
-                             $tmpinst = $modops->get_module_instance($moduledetails['name']);
-                            if( $tmpinst && $brief == 0 ) {
-                                $version = $tmpinst->GetVersion();
-                                if( version_compare($moduledetails['version'],$version) < 0 ) {
-                                     throw new \RuntimeException($this->_mod->Lang('err_xml_oldermodule'));
-                                }
-                                else if (version_compare($moduledetails['version'],$version) == 0 && $overwrite == 0 ) {
-                                     throw new \RuntimeException($this->_mod->Lang('err_xml_sameversion'));
-                                }
-                            }
-                            break;
-
-                        case 'MINCMSVERSION':
-                             $name = $reader->localName;
-                             $reader->read();
-                            if( $brief == 0 && version_compare(CMS_VERSION,$reader->value) < 0 ) {
-                                throw new \CmsLogicException($this->_mod->Lang('err_xml_moduleincompatible'));
-                            }
-                             $moduledetails[$name] = $reader->value;
-                            break;
-
-                        case 'MAXCMSVERSION':
-                        case 'DESCRIPTION':
-                        case 'FILENAME':
-                        case 'ISDIR':
-                             $name = $reader->localName;
-                             $reader->read();
-                             $moduledetails[$name] = $reader->value;
-                            break;
-
-                        case 'HELP':
-                        case 'ABOUT':
-                             $name = $reader->localName;
-                             $reader->read();
-                             $moduledetails[$name] = base64_decode($reader->value);
-                            break;
-
-                        case 'REQUIREDNAME':
-                             $reader->read();
-                             $requires['name'] = $reader->value;
-                            break;
-
-                        case 'REQUIREDVERSION':
-                             $reader->read();
-                             $requires['version'] = $reader->value;
-                            break;
-
-                        case 'DATA':
-                             $reader->read();
-                             $moduledetails['filedata'] = $reader->value;
-                            break;
+            case \XMLReader::ELEMENT:
+                switch( strtoupper($reader->localName) ) {
+                case 'NAME':
+                    $reader->read();
+                    $moduledetails['name'] = $reader->value;
+                    $loaded = $modops->GetLoadedModules();
+                    // check if this module is already installed
+                    if( isset( $loaded[$moduledetails['name']] ) && $overwrite == 0 && $brief == 0 ) {
+                        throw new \CmsLogicException($this->_mod->Lang('err_xml_moduleinstalled'));
                     }
-				                break;
+                    break;
 
-                case \XMLReader::END_ELEMENT:
-                    switch( strtoupper($reader->localName) ) {
-                        case 'REQUIRES':
-                            if( count($requires) == 2 ) {
-                                if( !isset( $moduledetails['requires'] ) ) $moduledetails['requires'] = array();
-                                $moduledetails['requires'][] = $requires;
-                                $requires = array();
-			    }
-                            break;
+                case 'DTDVERSION':
+                    $reader->read();
+                    if( $reader->value != self::MODULE_DTD_VERSION ) throw new \CmsInvalidDataException($this->_mod->Lang('err_xml_dtdmismatch'));
+                    $havedtdversion = true;
+                    break;
 
-                        case 'FILE':
-                            if( !$brief ) {
-                                // finished a first file
-                                if( !isset( $moduledetails['name'] ) || !isset( $moduledetails['version'] ) ||
-                                    !isset( $moduledetails['filename'] ) || !isset( $moduledetails['isdir'] ) ) {
-                                    throw new \CmsInvalidDataException($this->Lang('err_xml_invalid'));
-                                }
+                case 'VERSION':
+                    $reader->read();
+                    $moduledetails['version'] = $reader->value;
+                    $tmpinst = $modops->get_module_instance($moduledetails['name']);
+                    if( $tmpinst && $brief == 0 ) {
+                        $version = $tmpinst->GetVersion();
+                        if( version_compare($moduledetails['version'],$version) < 0 ) {
+                            throw new \RuntimeException($this->_mod->Lang('err_xml_oldermodule'));
+                        }
+                        else if (version_compare($moduledetails['version'],$version) == 0 && $overwrite == 0 ) {
+                            throw new \RuntimeException($this->_mod->Lang('err_xml_sameversion'));
+                        }
+                    }
+                    break;
 
-                                // ready to go
-                                $moduledir=$dir.DIRECTORY_SEPARATOR.$moduledetails['name'];
-                                $filename=$moduledir.$moduledetails['filename'];
-                                if( !file_exists( $moduledir ) ) {
-                                    if( !@mkdir( $moduledir ) && !is_dir( $moduledir ) ) {
-                                        throw new \CmsFileSystemException(lang('errorcantcreatefile').': '.$moduledir);
-                                    }
-                                }
-                                else if( $moduledetails['isdir'] ) {
-                                    if( !@mkdir( $filename ) && !is_dir( $filename ) ) {
-                                        throw new \CmsFileSystemException(lang('errorcantcreatefile').': '.$filename);
-                                    }
-                                }
-                                else {
-                                    $data = $moduledetails['filedata'];
-                                    if( strlen( $data ) ) $data = base64_decode( $data );
-                                    $fp = @fopen( $filename, "w" );
-                                    if( !$fp ) throw new \CmsFileSystemException(lang('errorcantcreatefile').' '.$filename);
-                                    if( strlen( $data ) ) @fwrite( $fp, $data );
-                                    @fclose( $fp );
-                                }
-                                unset( $moduledetails['filedata'] );
-                                unset( $moduledetails['filename'] );
-                                unset( $moduledetails['isdir'] );
-                                break;
+                case 'MINCMSVERSION':
+                    $name = $reader->localName;
+                    $reader->read();
+                    if( $brief == 0 && version_compare(CMS_VERSION,$reader->value) < 0 ) {
+                        throw new \CmsLogicException($this->_mod->Lang('err_xml_moduleincompatible'));
+                    }
+                    $moduledetails[$name] = $reader->value;
+                    break;
+
+                case 'MAXCMSVERSION':
+                case 'DESCRIPTION':
+                case 'FILENAME':
+                case 'ISDIR':
+                    $name = $reader->localName;
+                    $reader->read();
+                    $moduledetails[$name] = $reader->value;
+                    break;
+
+                case 'HELP':
+                case 'ABOUT':
+                    $name = $reader->localName;
+                    $reader->read();
+                    $moduledetails[$name] = base64_decode($reader->value);
+                    break;
+
+                case 'REQUIREDNAME':
+                    $reader->read();
+                    $requires['name'] = $reader->value;
+                    break;
+
+                case 'REQUIREDVERSION':
+                    $reader->read();
+                    $requires['version'] = $reader->value;
+                    break;
+
+                case 'DATA':
+                    $reader->read();
+                    $moduledetails['filedata'] = $reader->value;
+                    break;
+                }
+                break;
+
+            case \XMLReader::END_ELEMENT:
+                switch( strtoupper($reader->localName) ) {
+                case 'REQUIRES':
+                    if( count($requires) == 2 ) {
+                        if( !isset( $moduledetails['requires'] ) ) $moduledetails['requires'] = array();
+                        $moduledetails['requires'][] = $requires;
+                        $requires = array();
+                    }
+                    break;
+
+                case 'FILE':
+                    if( !$brief ) {
+                        // finished a first file
+                        if( !isset( $moduledetails['name'] ) || !isset( $moduledetails['version'] ) ||
+                            !isset( $moduledetails['filename'] ) || !isset( $moduledetails['isdir'] ) ) {
+                            throw new \CmsInvalidDataException($this->Lang('err_xml_invalid'));
+                        }
+
+                        // ready to go
+                        $moduledir=$dir.DIRECTORY_SEPARATOR.$moduledetails['name'];
+                        $filename=$moduledir.$moduledetails['filename'];
+                        if( !file_exists( $moduledir ) ) {
+                            if( !@mkdir( $moduledir ) && !is_dir( $moduledir ) ) {
+                                throw new \CmsFileSystemException(lang('errorcantcreatefile').': '.$moduledir);
                             }
-                            break;
+                        }
+                        else if( $moduledetails['isdir'] ) {
+                            if( !@mkdir( $filename ) && !is_dir( $filename ) ) {
+                                throw new \CmsFileSystemException(lang('errorcantcreatefile').': '.$filename);
+                            }
+                        }
+                        else {
+                            $data = $moduledetails['filedata'];
+                            if( strlen( $data ) ) $data = base64_decode( $data );
+                            $fp = @fopen( $filename, "w" );
+                            if( !$fp ) throw new \CmsFileSystemException(lang('errorcantcreatefile').' '.$filename);
+                            if( strlen( $data ) ) @fwrite( $fp, $data );
+                            @fclose( $fp );
+                        }
+                        unset( $moduledetails['filedata'] );
+                        unset( $moduledetails['filename'] );
+                        unset( $moduledetails['isdir'] );
+                        break;
+                    }
+                    break;
+                }
             }
         } // while
 
@@ -206,7 +207,7 @@ class operations
     {
         // get a file list
         $filecount = 0;
-	$dir = $modinstance->GetModulePath();
+        $dir = $modinstance->GetModulePath();
         if( !is_writable( $dir ) ) throw new \CmsFileSystemException(lang('errordirectorynotwritable'));
 
         // generate the moduleinfo.ini file
