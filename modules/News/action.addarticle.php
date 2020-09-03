68169d859ac2aaa8aa344183cf0085fc6bab123d
rolf1 <rolf1_old@cmsmadesimple.org>
2020-09-03 17:00:32 +0000

    Fix BR #12322
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@12535 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/News/action.addarticle.php
+++ modules/News/action.addarticle.php
@@ -24,7 +24,7 @@ $usedcategory = isset($params['category']) ? $params['category'] : $this->GetPre
 $useexp       = isset($params['useexp']) ? 1: 0;
 $searchable   = isset($params['searchable']) ? (int)$params['searchable'] : 1;
 $news_url     = isset($params['news_url']) ? $params['news_url'] : '';
-$extra        = isset($params['extra']) ? trim($params['extra']) : '';
+$extra        = isset($params['extra']) ? trim(strip_tags($params['extra'])) : '';
 $title        = isset($params['title']) ? trim(strip_tags($params['title'])) : '';
 $ndays        = (int)$this->GetPreference('expiry_interval', 180);
 
