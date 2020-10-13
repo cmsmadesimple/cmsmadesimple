fa6472b9126888d86036f8ae5b25316ab8746b4b
JoMorg <JoMorg_old@cmsmadesimple.org>
2020-10-13 15:56:03 +0000

    Bumped core release version and name it; bumped installer version; updated CHANGELOG;
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@12588 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -21,8 +21,8 @@
 # Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
 #---------------------------------------------------------------------------
 
-$CMS_VERSION = "2.2.15.b.3";
-$CMS_VERSION_NAME = "NOT SET";
+$CMS_VERSION = "2.2.15";
+$CMS_VERSION_NAME = "Bonaventure";
 $CMS_SCHEMA_VERSION = '202';
 
 define('CMS_VERSION', $CMS_VERSION);
