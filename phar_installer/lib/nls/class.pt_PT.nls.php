b9c69370fb33669c29a556920f314452fa1a2d79
rolf1 <rolf1_old@cmsmadesimple.org>
2021-03-10 17:47:58 +0000

    Add Russian NLS file to installer. Thanks Yuri Haperski.
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/3.0-dev@12599 3d254a34-79dc-0310-9e5f-be208747d8a0

--- phar_installer/lib/nls/class.pt_PT.nls.php
+++ phar_installer/lib/nls/class.pt_PT.nls.php
@@ -4,17 +4,19 @@ namespace __appbase;
 
 final class pt_PT_nls extends nls
 {
-  public function __construct()
-  {
-    $this->_fullname = 'Portuguese';
-    $this->_display = 'Portugu&ecirc;s';
-    $this->_isocode = 'pt';
-    $this->_locale = 'pt_PT';
-    $this->_encoding = 'UTF-8';
-    $this->_aliases = 'portugguese,pt_PT.ISO8859-1';
-  }  
-  
-  
+
+	public function __construct()
+	{
+		$this->_fullname = 'Portuguese';
+		$this->_display = 'Portugu&ecirc;s';
+		$this->_isocode = 'pt';
+		$this->_locale = 'pt_PT';
+		$this->_encoding = 'UTF-8';
+		$this->_aliases = 'portugguese,pt_PT.ISO8859-1';
+	}
+
 } // end of class
 
-?>
+#
+# EOF
+#
