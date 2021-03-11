88b8d62abf5c49953fc6aee099bf8e9550df59e0
rolf1 <rolf1_old@cmsmadesimple.org>
2021-03-11 14:31:48 +0000

    Clean up
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/3.0-dev@12614 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/Navigator/method.uninstall.php
+++ modules/Navigator/method.uninstall.php
@@ -1,26 +1,15 @@
 <?php
-#BEGIN_LICENSE
-#-------------------------------------------------------------------------
-# Module: Navigator (c) 2013 by Robert Campbell
-#         (calguy1000@cmsmadesimple.org)
-#  An module for CMS Made Simple to allow building hierarchical navigations.
-#
-#-------------------------------------------------------------------------
-# CMS - CMS Made Simple is (c) 2005 by Ted Kulp (wishy@cmsmadesimple.org)
-# Visit our homepage at: http://www.cmsmadesimple.org
-#
-#-------------------------------------------------------------------------
-#
+#---------------------------------------------------------------------------
+# CMS Made Simple - Power for the professional, Simplicity for the end user.
+# (c) 2004 - 2011 by Ted Kulp
+# (c) 2011 - 2018 by the CMS Made Simple Development Team
+# (c) 2018 and beyond by the CMS Made Simple Foundation
+# This project's homepage is: https://www.cmsmadesimple.org
+#---------------------------------------------------------------------------
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-#
-# However, as a special exception to the GPL, this software is distributed
-# as an addon module to CMS Made Simple.  You may not use this software
-# in any Non GPL version of CMS Made simple, or in any version of CMS
-# Made simple that does not indicate clearly and obviously in its admin
-# section that the site was built with CMS Made simple.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -30,10 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 # Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
-#-------------------------------------------------------------------------
-#END_LICENSE
-#$Id: News.module.php 2114 2005-11-04 21:51:13Z wishy $
+#---------------------------------------------------------------------------
 
 $this->RemovePreference();
 $this->DeleteTemplate();
@@ -65,4 +51,3 @@ catch( CmsException $e ) {
 #
 # EOF
 #
-?>
