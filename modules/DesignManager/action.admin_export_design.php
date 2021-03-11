6e4247108730d81135c669f2a263875dd3447029
rolf1 <rolf1_old@cmsmadesimple.org>
2021-03-11 12:41:20 +0000

    Clean up
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/3.0-dev@12609 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/DesignManager/action.admin_export_design.php
+++ modules/DesignManager/action.admin_export_design.php
@@ -1,8 +1,11 @@
 <?php
-#-------------------------------------------------------------------------
-# Module: AdminSearch - A CMSMS addon module to provide template management.
-# (c) 2012 by Robert Campbell <calguy1000@cmsmadesimple.org>
-#
+#---------------------------------------------------------------------------
+# CMS Made Simple - Power for the professional, Simplicity for the end user.
+# (c) 2004 - 2011 by Ted Kulp
+# (c) 2011 - 2018 by the CMS Made Simple Development Team
+# (c) 2018 and beyond by the CMS Made Simple Foundation
+# This project's homepage is: https://www.cmsmadesimple.org
+#---------------------------------------------------------------------------
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -16,8 +19,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 # Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
-#
-#-------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
 if( !isset($gCms) ) exit;
 if( !$this->CheckPermission('Manage Designs') ) return;
 
@@ -166,4 +169,3 @@ catch( \Exception $e ) {
 #
 # EOF
 #
-?>
