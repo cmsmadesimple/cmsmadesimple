4016ea093ba21e85aa4ec8b6820a4eae7e83ac6c
JoMorg <JoMorg_old@cmsmadesimple.org>
2021-11-01 23:27:19 +0000

    stupid typo
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@12641 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -21,7 +21,7 @@
 # Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
 #---------------------------------------------------------------------------
 
-$CMS_VERSION = "2.2.16".beta.1;
+$CMS_VERSION = "2.2.16.beta.1";
 $CMS_VERSION_NAME = "No Name Set";
 $CMS_SCHEMA_VERSION = '202';
 
