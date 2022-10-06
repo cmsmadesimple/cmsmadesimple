d0c360910100f04484310c1591c955208aad847b
tomphantoo <omphantoo_old@cmsmadesimple.org>
2022-10-06 23:29:24 +0000

    remove incorrect reference on method argument
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@12838 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/plugins/function.file_url.php
+++ lib/plugins/function.file_url.php
@@ -1,5 +1,5 @@
 <?php
-function smarty_function_file_url($params,&$template)
+function smarty_function_file_url($params, $template)
 {
     $config = \cms_config::get_instance();
     $dir = $config['uploads_path'];
@@ -9,23 +9,22 @@ function smarty_function_file_url($params,&$template)
 
     if( !$file ) {
         trigger_error('file_url plugin: invalid file parameter');
-        return;
+        return '';
     }
     if( $add_dir ) {
         if( startswith( $add_dir, '/') ) $add_dir = substr($add_dir,1);
         $dir = $dir.'/'.$add_dir;
         if( !is_dir($dir) || !is_readable($dir) ) {
             trigger_error("file_url plugin: dir=$add_dir invalid directory name specified");
-            return;
+            return '';
         }
     }
 
-    $out = null;
     if( $file ) {}
     $fullpath = $dir.'/'.$file;
     if( !is_file($fullpath) || !is_readable($fullpath) ) {
         // no error log here.
-        return;
+        return '';
     }
 
     // convert it to a url
@@ -35,7 +34,7 @@ function smarty_function_file_url($params,&$template)
 
     if( $assign ) {
         $template->assign($assign,$out);
-        return;
+        return '';
     }
     return $out;
 }
