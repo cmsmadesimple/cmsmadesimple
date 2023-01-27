b1cbe874073d036df56371cdca6ec890fcbb05ca
JoMorg <JoMorg_old@cmsmadesimple.org>
2023-01-27 19:09:02 +0000

    oops
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/2.2.17-b.4-JoMorg@12865 3d254a34-79dc-0310-9e5f-be208747d8a0

--- CMSMS-2-4-DEV-MODS/scripts/oldback.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for file in  'basic_tests.php' 'except_test.php'  'subversion_test.php'  'year_test1.php' 'year_test2.php'; do
- mv -f $file'-old' $file
-done
--- CMSMS-2-4-DEV-MODS/tests/year_test2.php
+++
@@ -1,2 +0,0 @@
-// Copyright (C) 2004-2011 Ted Kulp <ted@cmsmadesimple.org>
-//Copyright (C) 2011-2021 CMS Made Simple Foundation <foundation@cmsmadesimple.org>
