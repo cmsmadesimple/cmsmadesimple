96fad96575b8fb901cdd5c84e518eba172c325bc
tomphantoo <omphantoo_old@cmsmadesimple.org>
2023-05-29 12:10:36 +0000

    give this thing a name
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@12900 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -21,8 +21,8 @@
 # Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
 #---------------------------------------------------------------------------
 
-$CMS_VERSION = "2.2.17-b.3";
-$CMS_VERSION_NAME = "NOT SET";
+$CMS_VERSION = '2.2.17';
+$CMS_VERSION_NAME = 'Iqaluit';
 $CMS_SCHEMA_VERSION = '202';
 
 define('CMS_VERSION', $CMS_VERSION);
