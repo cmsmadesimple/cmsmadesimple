c0e48665f42599ad310066827420fad7f4120d10
tomphantoo <omphantoo_old@cmsmadesimple.org>
2023-06-06 11:57:51 +0000

    conform to CMSMS2 content-tree data structure
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@12945 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/MenuManager/action.default.php
+++ modules/MenuManager/action.default.php
@@ -3,7 +3,6 @@ if (!isset($gCms)) exit;
 
 debug_buffer('', 'Start of Menu Manager Display');
 
-$template = null;
 if( isset($params['template']) ) {
     $template = trim($params['template']);
 }
@@ -31,13 +30,13 @@ if( !$tpl->isCached() ) {
     $rootnode = null;
     $prevdepth = 1;
 
-    if (isset($params['childrenof']) ) {
+    if( isset($params['childrenof']) ) {
         $parent = $hm->sureGetNodeByAlias($params['childrenof']);;
         if( $parent ) {
-            if( !is_array($rootnode) ) $rootnode = array();
             // get the children.
             $children = $parent->GetChildren($deep);
             if( $children && is_array($children) ) {
+                if( !is_array($rootnode) ) { $rootnode = array(); }
                 foreach( $children as $onechild ) {
                     $obj = $onechild->GetContent();
                     if( is_object($obj) && $obj->Active() &&
@@ -48,62 +47,56 @@ if( !$tpl->isCached() ) {
             }
         }
     }
-    else if (isset($params['start_page']) || isset($params['start_element'])) {
-        if (isset($params['start_page']))
+    else if( isset($params['start_page']) || isset($params['start_element']) ) {
+        if( isset($params['start_page']) ) {
             $rootnode = $hm->sureGetNodeByAlias($params['start_page']);
-        else
+        }
+        else {
             $rootnode = $hm->getNodeByHierarchy($params['start_element']);
-
-        if (isset($rootnode)) {
+        }
+        if( $rootnode ) {
             $content = $rootnode->GetContent();
-            if (isset($content)) {
-                if (isset($params['show_root_siblings']) && $params['show_root_siblings'] == '1') {
-                    if ($rootnode->getLevel() == 0) {
-                        $rootnode = $rootnode->getParentNode();
-                        $prevdepth = 1;
-                    }
-                    else {
-                        // Set original depth first before getting parent node
-                        // This is slightly hackish, but it works nicely
-                        // +1 and +2 fix HM changes of root node level
-                        // even more hackish ;)
-                        $origdepth = $rootnode->getLevel()+1;
-                        $rootnode  = $rootnode->getParentNode();
-                        $prevdepth = $rootnode->getLevel()+2;
-                    }
+            if( $content ) {
+                if( isset($params['show_root_siblings']) && $params['show_root_siblings'] == '1' ) {
+                    // Set original depth first before getting parent node
+                    $origdepth = substr_count($rootnode->getHierarchy(), '.') + 1;
+// OR               $origdepth = substr_count($content->Hierarchy(), '.') + 1;
+                    $rootnode  = $rootnode->getParent();
+                    $prevdepth = substr_count($rootnode->getHierarchy(), '.') + 1;
                 }
                 else {
                     //Show a page if it's active and set to show in menu...
                     //Or if show_all is set and the page isn't a "system" page
-                    if ($content->Active() &&
+                    if( $content->Active() &&
                         ($content->ShowInMenu() || (isset($params['show_all']) && $params['show_all'] == 1 && !$content->IsSystemPage()))) {
-                        $prevdepth = count(explode('.', $content->Hierarchy()));
-                        $this->FillNode($content, $rootnode, $nodelist, $count, $origdepth, $prevdepth, $deep, $params);
-                        if (isset($params['number_of_levels']) && $params['number_of_levels'] == '1')
+                        $origdepth = count(explode('.', $content->Hierarchy()));
+                        $this->FillNode($content, $rootnode, $nodelist, $count, $prevdepth, $origdepth, $deep, $params);
+                        if( isset($params['number_of_levels']) && $params['number_of_levels'] == '1' ) {
                             $getchildren = false;
+                        }
                     }
                 }
             }
         }
     }
-    else if (isset($params['start_level']) && intval($params['start_level']) > 1) {
+    else if( isset($params['start_level']) && (int)$params['start_level'] > 1 ) {
         $curcontent = $gCms->get_content_object();
-        if( $curcontent )  {
-            $properparentpos = $this->nthPos($curcontent->Hierarchy() . '.', '.', intval($params['start_level']) - 1);
-            if ($properparentpos > -1) {
-                $prevdepth = intval($params['start_level']);
+        if( $curcontent ) {
+            $properparentpos = $this->nthPos($curcontent->Hierarchy() . '.', '.', (int)$params['start_level'] - 1);
+            if( $properparentpos > -1 ) {
+                $prevdepth = (int)$params['start_level'];
                 $rootnode = $hm->getNodeByHierarchy(substr($curcontent->Hierarchy(), 0, $properparentpos));
             }
         }
     }
-    else if (isset($params['items'])) {
-        if( !isset($params['number_of_levels']) ){
+    else if( isset($params['items']) ) {
+        if( !isset($params['number_of_levels']) ) {
             $getchildren = false;
             $params['number_of_levels'] = 1;
         }
 
         $items = explode(',', $params['items']);
-        if ($items) {
+        if( $items ) {
             foreach ($items as $oneitem) {
                 $curnode = $hm->sureGetNodeByAlias(trim($oneitem));
                 if( !$curnode ) continue;
@@ -116,7 +109,7 @@ if( !$tpl->isCached() ) {
                 }
                 else {
                     $prevdepth = 1;
-                    $mnode = $this->FillNode($content,$curnode,$nodelist,$count,$prevdepth,1,$deep,$params);
+                    $mnode = $this->FillNode($content, $curnode, $nodelist, $count, $prevdepth, 1, $deep, $params);
                     $mnode->depth = 1;
                 }
             }
@@ -131,7 +124,7 @@ if( !$tpl->isCached() ) {
 
     $showparents = array();
 
-    if (isset($params['collapse']) && $params['collapse'] == '1') {
+    if( isset($params['collapse']) && $params['collapse'] == '1' ) {
         $newpos = '';
         $content = CmsApp::get_instance()->get_content_object();
         if( $content ) {
@@ -145,20 +138,21 @@ if( !$tpl->isCached() ) {
     }
 
     // See if origdepth was ever set...  if not, then get it from the prevdepth set earlier
-    if ($origdepth == 0) $origdepth = $prevdepth;
+    if( $origdepth == 0 ) $origdepth = $prevdepth;
 
-    if (isset($rootnode) && $getchildren) {
+    if( isset($rootnode) && $getchildren ) {
         if( $rootnode && is_array($rootnode) ) {
             $first = 1;
             for( $n = 0; $n < count($rootnode); $n++ ) {
                 $onenode = $rootnode[$n];
                 $content = $onenode->GetContent();
                 if( $first ) {
-                    $prevdepth = $origdepth = $onenode->GetLevel() + 1;
+                    $prevdepth = $origdepth = substr_count($onenode->getHierarchy(), '.') + 1;
+//OR                $prevdepth = $origdepth = substr_count($content->Hierarchy(), '.') + 1;
                     $first = 0;
                 }
                 if( $content ) {
-                    $mnode = $this->FillNode($content, $onenode, $nodelist, $count, $prevdepth, $prevdepth, $deep);
+                    $mnode = $this->FillNode($content, $onenode, $nodelist, $count, $prevdepth, $origdepth, $deep);
                     if( $n == 0 ) {
                         $mnode->first = 1;
                     }
@@ -183,7 +177,7 @@ if( !$tpl->isCached() ) {
         }
     }
 
-    if ($nodelist) {
+    if( $nodelist ) {
         $tpl->assign('menuparams',$params);
         $tpl->assign('count', count($nodelist));
         $tpl->assign('nodelist', $nodelist);
