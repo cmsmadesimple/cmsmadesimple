0f09913e2a80a28f363d9f15a91cca9d5a64e276
tomphantoo <omphantoo_old@cmsmadesimple.org>
2023-06-12 00:51:56 +0000

    block page cacheing
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@13023 3d254a34-79dc-0310-9e5f-be208747d8a0

--- phar_installer/app/lib/class.wizard_step.php
+++ phar_installer/app/lib/class.wizard_step.php
@@ -65,6 +65,13 @@ abstract class wizard_step extends parent_step
 
   protected function display()
   {
+      header('Content-Type:text/html; charset=UTF-8');
+      // block page cacheing
+      header('Expires: Thu, 1 Jan 1970 00:00:00 GMT');
+      header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+      header('Cache-Control: post-check=0, pre-check=0',false);
+      header('Pragma: no-cache');
+
       smarty()->assign('wizard_steps',$this->get_wizard()->get_nav())
         ->assign('title',$this->get_primary_title());
   }
