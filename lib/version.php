6e3f466ec29f09daa37ecc89a9695dd37752028d
JoMorg <JoMorg_old@cmsmadesimple.org>
2023-06-12 16:31:41 +0000

    bumped version to final
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/branches/2.2.17-b.4-JoMorg@13033 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/version.php
+++ lib/version.php
@@ -21,8 +21,8 @@
 # Or read it online: http://www.gnu.org/licenses/licenses.html#GPL
 #---------------------------------------------------------------------------
 
-$CMS_VERSION = '2.2.17-b.8';
-$CMS_VERSION_NAME = 'NOT RELEASED';
+$CMS_VERSION = '2.2.17';
+$CMS_VERSION_NAME = 'Iqaluit';
 $CMS_SCHEMA_VERSION = '202';
 
 define('CMS_VERSION', $CMS_VERSION);
