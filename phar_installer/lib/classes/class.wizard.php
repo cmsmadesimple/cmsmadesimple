5614fb5771276cfcbdd5f5ab2ae660acc33ad088
JoMorg <JoMorg@3d254a34-79dc-0310-9e5f-be208747d8a0>
2024-04-21 17:50:36 +0000

    fixes url appending issue with installations on some hosts
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@13137 3d254a34-79dc-0310-9e5f-be208747d8a0

--- phar_installer/lib/classes/class.wizard.php
+++ phar_installer/lib/classes/class.wizard.php
@@ -170,17 +170,21 @@ class wizard
       $this->_init();
       $request = request::get();
       $url = $request->raw_server('REQUEST_URI');
+      
       $urlmain = \explode('?', $url);
 
       \parse_str($url, $parts);
       $parts[$this->_stepvar] = $this->cur_step() + 1;
       if( $parts[$this->_stepvar] > $this->num_steps() ) return '';
 
-      $tmp = array();
-      foreach( $parts as $k => $v ) {
-          $tmp[] = $k.'='.$v;
-      }
+      $tmp = [];
+    foreach($parts as $k => $v)
+    {
+      if($k == $urlmain[0]) { continue; }
+      $tmp[] = $k . '=' . $v;
+    }
       $url = $urlmain[0].'?' . \implode('&', $tmp);
+    
       return $url;
   }
 
