ee80fcf5a56e53b53f74e07d5be42f3bfb3e3f77
jdearaujo <jdearaujo@3d254a34-79dc-0310-9e5f-be208747d8a0>
2024-12-11 10:29:05 +0000

    BR12658 - removed redundant string & other language file tweaks
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@13207 3d254a34-79dc-0310-9e5f-be208747d8a0

--- lib/lang/tags/en_US.php
+++ lib/lang/tags/en_US.php
@@ -278,8 +278,8 @@ $lang['help_function_browser_lang'] = <<<EOT
   <p>This plugin detects and outputs the language that the users browser accepts, and cross references it with a list of allowed languages to determine a language value for the session.</p>
 <h3>How do I use it?</h3>
 <p>Insert the tag early into your page template <em>(it can go above the &lt;head&gt; section if you want)</em> and provide it the name of the default language, and the accepted languages (only two character language names are accepted), then do something with the result.  i.e:</p>
-	     <pre><code>{browser_lang accepted=&quot;de,fr,en,es&quot; default=en assign=tmp}{session_put var=lang val=\$tmp}</code></pre>
-<p><em>({session_put} is a plugin provided by the CGSimpleSmarty module)</em></p>
+        <pre><code>{browser_lang accepted=&quot;de,fr,en,es&quot; default=en assign=tmp}{sess_put var=lang val=\$tmp}</code></pre>
+<p><em>({sess_put} is a plugin provided by the SmartyExt module)</em></p>
 <h3>What Parameters does it Take?</h3>
 <ul>
 <li><strong>accepted <em>(required)</em></strong><br/> - A comma separated list of two character language names that are accepted.</li>
@@ -365,7 +365,7 @@ $lang['help_function_page_attr'] = <<<EOT
       <li>extra1 - (string) The value of the extra1 attribute.</li>
       <li>extra2 - (string) The value of the extra2 attribute.</li>
       <li>extra3 - (string) The value of the extra3 attribute.</li>
-      <li>pageattr - (string) The value of the page specific smarty data attribute./li>
+      <li>pageattr - (string) The value of the page specific smarty data attribute.</li>
     </ul>
     <p><strong>Note:</strong> The list above is not inclusive.  You can also retrieve the unparsed contents of additional content blocks or properties added by third party modules.  The output is always a string</p>
   </li>
