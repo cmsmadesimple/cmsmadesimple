eb701ff2b41a9e68dc9aa54ee3290011007c48f6
JoMorg <JoMorg@3d254a34-79dc-0310-9e5f-be208747d8a0>
2025-08-12 11:46:07 +0000

    bumped version to final
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@13245 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/Search/Search.module.php
+++ modules/Search/Search.module.php
@@ -48,7 +48,7 @@ class Search extends CMSModule
     public function IsPluginModule() { return true; }
     public function HasAdmin() { return true; }
     public function HandlesEvents () { return true; }
-    public function GetVersion() { return '1.53'; }
+    public function GetVersion() { return '1.54'; }
     public function MinimumCMSVersion() { return '1.12'; }
     public function GetAdminDescription() { return $this->Lang('description'); }
     public function VisibleToAdminUser() { return $this->CheckPermission('Manage Search'); }
