48518dc9f87f738233ae8bfa9108bf5115858ea0
Magal <magal@pixelsolutions.biz>
2026-05-05 13:05:38 +0000

    Add module analytics tracking (#19)
    Track module install, upgrade, and uninstall events via API calls to
    https://api.cmsmadesimple.org/v1/modules/{module}/events
    
    - Non-blocking (3s timeout)
    - Fails silently
    - Tracks successful operations only
    - Modified: ModuleManager action files and modmgr_utils class
    
    From: Magal <magal@pixelsolutions.biz>
    
    git-svn-id: http://svn.cmsmadesimple.org/svn/cmsmadesimple/trunk@13266 3d254a34-79dc-0310-9e5f-be208747d8a0

--- modules/ModuleManager/lib/class.modmgr_utils.php
+++ modules/ModuleManager/lib/class.modmgr_utils.php
@@ -236,6 +236,32 @@ final class modmgr_utils
         if( $ts >= $new_ts ) return 'new';
     }
 
+    public static function track_module_event($module_name, $event_type, $module_version)
+    {
+        try {
+            global $CMS_VERSION;
+            $url = 'https://api.cmsmadesimple.org/v1/modules/' . urlencode($module_name) . '/events';
+            $data = json_encode([
+                'eventType' => $event_type,
+                'cmsVersion' => $CMS_VERSION,
+                'moduleVersion' => $module_version
+            ]);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+            curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, 60);
+            curl_exec($ch);
+            curl_close($ch);
+        } catch (Exception $e) {
+            // Silently fail - don't interrupt module operations
+        }
+    }
+
     public static function get_images()
     {
         // this is a bit ugly.
